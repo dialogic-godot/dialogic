de725023fb5b30cb018ab0251ac788992621a31f
Emilio Coppola <coppolaemilio@gmail.com>
2021-03-11 14:08:15 +0100

    new definitions editor finished?

--- addons/dialogic/Editor/DefinitionEditor/DefinitionEditor.gd
+++ addons/dialogic/Editor/DefinitionEditor/DefinitionEditor.gd
@@ -10,6 +10,10 @@ onready var nodes = {
 	'type': $VBoxContainer/HBoxContainer/VBoxContainer/TypeMenuButton,
 	'extra_editor': $VBoxContainer/HBoxContainer/ExtraInfo,
 	'value_editor': $VBoxContainer/HBoxContainer/Value,
+	'value': $VBoxContainer/HBoxContainer/Value/ValueInput,
+	'extra_title': $VBoxContainer/HBoxContainer/ExtraInfo/Title,
+	'extra_text': $VBoxContainer/HBoxContainer/ExtraInfo/Text,
+	'extra_extra': $VBoxContainer/HBoxContainer/ExtraInfo/Extra,
 }
 
 func _ready():
@@ -25,11 +29,21 @@ func load_definition(section):
 	nodes['name'].text = get_definition('name', 'Unnamed')
 	var type = get_definition('type', 0)
 	nodes['type'].select(type)
+	if type == 0:
+		nodes['value'].text = get_definition('value', '')
+	if type == 1:
+		nodes['extra_title'].text = get_definition('extra_title', '')
+		nodes['extra_text'].text = get_definition('extra_text', '')
+		nodes['extra_extra'].text = get_definition('extra_extra', '')
 	show_sub_editor(type)
 
 
 func reset_editor():
 	nodes['name'].text = ''
+	nodes['value'].text = ''
+	nodes['extra_title'].text = ''
+	nodes['extra_text'].text = ''
+	nodes['extra_extra'].text = ''
 	nodes['type'].select(get_definition('type', 0))
 
 
@@ -73,6 +87,7 @@ func new_definition():
 	if err == OK:
 		config.set_value(section, 'name', 'New definition')
 		config.set_value(section, 'type', 0)
+		config.set_value(section, 'value', '')
 		config.save(DialogicUtil.get_path('DEFINITIONS_FILE'))
 		master_tree.add_definition({'section': section,'name': 'New definition', 'type': 0}, true)
 	else:
@@ -85,5 +100,13 @@ func save_definition():
 		var err = config.load(DialogicUtil.get_path('DEFINITIONS_FILE'))
 		if err == OK:
 			config.set_value(current_section, 'name', nodes['name'].text)
-			config.set_value(current_section, 'type', nodes['type'].selected)
+			var type = nodes['type'].selected
+			config.set_value(current_section, 'type', type)
+			if type == 0:
+				config.set_value(current_section, 'value', nodes['value'].text)
+			if type == 1:
+				config.set_value(current_section, 'extra_title', nodes['extra_title'].text)
+				config.set_value(current_section, 'extra_text', nodes['extra_text'].text)
+				config.set_value(current_section, 'extra_extra', nodes['extra_extra'].text)
+			
 			config.save(DialogicUtil.get_path('DEFINITIONS_FILE'))
