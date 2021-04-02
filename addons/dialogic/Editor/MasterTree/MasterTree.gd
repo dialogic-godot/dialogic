b43e44d0341977634c40f27c7dc17590b875bc4c
Emilio Coppola <coppolaemilio@gmail.com>
2021-04-02 15:08:45 +0200

    Adding the folder icons again.

--- addons/dialogic/Editor/MasterTree/MasterTree.gd
+++ addons/dialogic/Editor/MasterTree/MasterTree.gd
@@ -32,22 +32,22 @@ func _ready():
 	timelines_tree = tree.create_item(root)
 	timelines_tree.set_selectable(0, false)
 	timelines_tree.set_text(0, "Timelines")
-	#timelines_tree.set_icon(0, get_icon("Folder", "EditorIcons"))
+	timelines_tree.set_icon(0, get_icon("Folder", "EditorIcons"))
 	
 	characters_tree = tree.create_item(root)
 	characters_tree.set_selectable(0, false)
 	characters_tree.set_text(0, "Characters")
-	#characters_tree.set_icon(0, get_icon("Folder", "EditorIcons"))
+	characters_tree.set_icon(0, get_icon("Folder", "EditorIcons"))
 
 	definitions_tree = tree.create_item(root)
 	definitions_tree.set_selectable(0, false)
 	definitions_tree.set_text(0, "Definitions")
-	#definitions_tree.set_icon(0, get_icon("Folder", "EditorIcons"))
+	definitions_tree.set_icon(0, get_icon("Folder", "EditorIcons"))
 	
 	themes_tree = tree.create_item(root)
 	themes_tree.set_selectable(0, false)
 	themes_tree.set_text(0, "Themes")
-	#themes_tree.set_icon(0, get_icon("Folder", "EditorIcons"))
+	themes_tree.set_icon(0, get_icon("Folder", "EditorIcons"))
 	
 	settings_tree = tree.create_item(root)
 	settings_tree.set_selectable(0, true)
