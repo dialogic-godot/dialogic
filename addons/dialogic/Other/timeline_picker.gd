8a99402ca823634098473b5256b0e4bfbb6ce18f
ellogwen <private@development.local>
2021-04-03 10:22:32 +0200

    disable button on non selected

--- addons/dialogic/Other/timeline_picker.gd
+++ addons/dialogic/Other/timeline_picker.gd
@@ -14,6 +14,9 @@ var current_value = ''
 # A guard against internal changes when the property is updated.
 var updating = false
 
+# @Override
+func get_tooltip_text():
+	return "Click to select a Dialogic timeline.\nPress the tool button to directly switch to the editor"
 
 func _init():
 	# setup controls
@@ -24,6 +27,7 @@ func _init():
 	container.add_child(edit_button)
 	edit_button.hint_tooltip = "Edit Timeline"	
 	edit_button.icon = preload("res://addons/dialogic/Images/Tools.svg")
+	edit_button.disabled = true
 	
 	# Add the container as a direct child
 	add_child(container)
@@ -35,6 +39,7 @@ func _init():
 	timelines_dropdown.connect("about_to_show", self, "_about_to_show_menu")
 	timelines_dropdown.get_popup().connect("index_pressed", self, '_on_timeline_selected')
 	edit_button.connect("pressed", self, "_on_editTimelineButton_pressed")
+
 
 func _about_to_show_menu():
 	# Ignore the signal if the property is currently being updated.
@@ -49,11 +54,14 @@ func _about_to_show_menu():
 		timelines_dropdown.get_popup().set_item_metadata(index, {'file': c['file'], 'color': c['color']})
 		index += 1
 
+
 func _on_timeline_selected(index):
 	var text = timelines_dropdown.get_popup().get_item_text(index)
 	var metadata = timelines_dropdown.get_popup().get_item_metadata(index)
 	current_value = metadata['file']
 	timelines_dropdown.text = text
+	timelines_dropdown.hint_tooltip = text
+	_update_edit_button(current_value)
 	emit_changed(get_edited_property(), current_value)
 	
 	
@@ -65,15 +73,32 @@ func _on_editTimelineButton_pressed():
 func update_property():
 	# Read the current value from the property.
 	var new_value = get_edited_object()[get_edited_property()]
+	_update_edit_button(new_value)
+	
 	if (new_value == current_value):
 		return
-
+		
 	# Update the control with the new value.
 	updating = true
 	current_value = new_value
 	# Checking for the display name
+	timelines_dropdown.text = ''
+	
+	if (current_value == ''):
+		timelines_dropdown.hint_tooltip = 'Click to select a timeline'
+		
 	for c in DialogicUtil.get_timeline_list():
 		if c['file'] == current_value:
 			timelines_dropdown.text = c['name']
 			timelines_dropdown.hint_tooltip = c['name']
+			
 	updating = false
+	
+	_update_edit_button(current_value)
+	
+	
+func _update_edit_button(value):
+	if (value == ''):
+		edit_button.disabled = true
+	else:
+		edit_button.disabled = false
