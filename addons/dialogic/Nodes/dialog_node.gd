090d71e80094e25f906be3c5f411596d47adae8c
Dex <dav.medrin@gmail.com>
2021-05-08 11:28:57 -0500

    Update dialog_node.gd
    This way you can warn the user that he did not add his node to a canvas layer and that it probably won't show up in the game as expects. 
    
    It is not an automatic way to solve the problem, but a way to tell you what the problem is.

--- addons/dialogic/Nodes/dialog_node.gd
+++ addons/dialogic/Nodes/dialog_node.gd
@@ -74,6 +74,10 @@ func _ready():
 		if preview:
 			_init_dialog()
 	else:
+		# Copied
+		if not(get_parent() is CanvasLayer):
+			push_warning("[Dialogic] You didn't add this node to a CanvasLayer. If this was intentional, you can ignore this warning."))
+		
 		_init_dialog()
 
 
