b06806450646375b42284afbff47a773441887c2
Jowan-Spooner <42868150+Jowan-Spooner@users.noreply.github.com>
2021-05-29 02:42:48 +0200

    BackgroundMusic fixes (#318)
    * Fixed Dependecies
    
    I moved some scenes in the last minute. Stupid me. I hope it is fixed now.
    
    * Revert "Fixed Dependecies"
    
    This reverts commit 9e7bcd8dbb29b69aa6373ad5553ebd686f437de6.
    
    * Auto stash before merge of "main" and "upstream/main"
    
    * Update Changelog
    
    To contain the changes I made during the last week.
    
    * Revert "Auto stash before merge of "main" and "upstream/main""
    
    This reverts commit 8e1699d08a204b65e3f292067b2c4e870aaee1c7.
    
    * Add the new styles to the remaining pickers
    
    TimelinePicker, ThemePicker, OperationPicker, GlossaryPicker. It also removes the [] around the items, except for the operation picker. I wasn't sure if it wouldn't make readability harder having three similar buttons next to each other, so I kept the [] there. 
    
    And it adds a "Bus:" label to the AudioPicker, because I thought it would make it cleaner.
    
    * BackgroundMusic fixes
    
    The Background music can now fade more then once (previously the old track kept playing and so no new music was faded in).
    
    Fading now is done on an exponantial curve to be more linear. That might be confusing but somehow the decibel scale is logarithmic. This means more fluid transitions.
    
    I still need to test things better though. Also a tranisition will still fail if the previous one is still in progress.

--- addons/dialogic/Nodes/BackgroundMusic.gd
+++ addons/dialogic/Nodes/BackgroundMusic.gd
@@ -6,7 +6,11 @@ onready var _track2 := $Track2
 
 var current_path = ""
 
+func _ready():
+	$Tween.connect("tween_completed", self, "_on_Tween_tween_completed")
+
 func crossfade_to(path: String, audio_bus:String, volume:float, fade_length: float) -> void:
+	# find a better solution for this
 	if _track1.playing and _track2.playing:
 		return
 	
@@ -24,8 +28,8 @@ func crossfade_to(path: String, audio_bus:String, volume:float, fade_length: flo
 	fade_in_track.volume_db = -60
 	
 	
-	$Tween.interpolate_property(fade_out_track, "volume_db", null, -60, fade_length, Tween.TRANS_LINEAR)
-	$Tween.interpolate_property(fade_in_track, "volume_db", -60, volume, fade_length, Tween.TRANS_LINEAR)
+	$Tween.interpolate_property(fade_out_track, "volume_db", null, -60, fade_length, Tween.TRANS_EXPO)
+	$Tween.interpolate_property(fade_in_track, "volume_db", -60, volume, fade_length, Tween.TRANS_EXPO)
 	$Tween.start()
 	
 	# in case the audio is already playing we will attempt a fade into the new one from the current position
@@ -38,6 +42,12 @@ func crossfade_to(path: String, audio_bus:String, volume:float, fade_length: flo
 
 func fade_out(fade_length:float = 1) -> void:
 	current_path = ""
-	$Tween.interpolate_property(_track1, "volume_db", null, -60, fade_length, Tween.TRANS_LINEAR)
-	$Tween.interpolate_property(_track2, "volume_db", null, -60, fade_length, Tween.TRANS_LINEAR)
+	$Tween.interpolate_property(_track1, "volume_db", null, -60, fade_length, Tween.TRANS_EXPO)
+	$Tween.interpolate_property(_track2, "volume_db", null, -60, fade_length, Tween.TRANS_EXPO)
 	$Tween.start()
+
+func _on_Tween_tween_completed(object, key):
+	# if the stream was faded out
+	if object.volume_db == -60:
+		object.playing = false
+		object.stream = null
