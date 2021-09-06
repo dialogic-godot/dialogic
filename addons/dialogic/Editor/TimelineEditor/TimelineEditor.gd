218f7666754df8bb0b3ce67f0d455035c53b3b4f
Emilio Coppola <coppolaemilio@gmail.com>
2021-09-07 01:59:38 +0200

    only scrolling to pieces created by clicking on the button

--- addons/dialogic/Editor/TimelineEditor/TimelineEditor.gd
+++ addons/dialogic/Editor/TimelineEditor/TimelineEditor.gd
@@ -607,6 +607,7 @@ func _create_event_button_pressed(button_name):
 	TimelineUndoRedo.add_do_method(self, "create_event", button_name, {'no-data': true}, true, at_index, true)
 	TimelineUndoRedo.add_undo_method(self, "remove_events_at_index", at_index, 1)
 	TimelineUndoRedo.commit_action()
+	scroll_to_piece(at_index)
 	indent_events()
 
 
@@ -791,8 +792,6 @@ func create_event(scene: String, data: Dictionary = {'no-data': true} , indent: 
 
 	piece.connect("option_action", self, '_on_event_options_action', [piece])
 	piece.connect("gui_input", self, '_on_event_block_gui_input', [piece])
-
-	scroll_to_piece(piece)
 	
 	events_warning.visible = false
 	if auto_select:
@@ -1042,9 +1041,9 @@ func save_timeline() -> void:
 ## *****************************************************************************
 
 # Scrolling
-func scroll_to_piece(piece) -> void:
+func scroll_to_piece(piece_index) -> void:
 	var height = 0
-	for i in range(0, piece.get_index()):
+	for i in range(0, piece_index):
 		height += $TimelineArea/TimeLine.get_child(i).rect_size.y
 	$TimelineArea.scroll_vertical = height
 
