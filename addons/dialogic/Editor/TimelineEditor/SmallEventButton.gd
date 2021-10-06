459647296ff11ec4ce7d7ab10b1bdb941428c08f
Emilio Coppola <coppolaemilio@gmail.com>
2021-10-06 22:24:18 +0200

    Updating the icons style

--- addons/dialogic/Editor/TimelineEditor/SmallEventButton.gd
+++ addons/dialogic/Editor/TimelineEditor/SmallEventButton.gd
@@ -3,29 +3,24 @@ extends Button
 
 export(String) var visible_name = ""
 export (String) var event_id = 'dialogic_099'
+export (Color) var event_color = Color('#48a2a2a2')
 export(Texture) var event_icon = null setget set_icon
 
 
 func _ready():
-	$HBox/Label.text = visible_name
+	$Panel.self_modulate = event_color
+	self_modulate = Color(1,1,1)
+	if visible_name != '':
+		text = '  ' + visible_name
 	hint_tooltip = DTS.translate(hint_tooltip)
 	var _scale = get_constant("inspector_margin", "Editor")
 	_scale = _scale * 0.125
 	rect_min_size = Vector2(30,30)
-	if _scale == 1.25:
-		rect_min_size = Vector2(30,30)
-	if _scale == 1.5:
-		rect_min_size = Vector2(30,30)
-	if _scale == 1.75:
-		rect_min_size = Vector2(60,60)
-	if _scale == 2:
-		rect_min_size = Vector2(60,60)
-	
-	$HBox/TextureRect.rect_min_size = rect_min_size -Vector2(2,2)
+	rect_min_size = rect_min_size * _scale
 
 
 func set_icon(texture):
-	$HBox/TextureRect.texture = texture
+	icon = texture
 	event_icon = texture
 
 
