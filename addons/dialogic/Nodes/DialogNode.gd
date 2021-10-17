d3a49c5c94b814c09303bb7a67fdc6e13573ab04
Jowan-Spooner <42868150+Jowan-Spooner@users.noreply.github.com>
2021-10-17 20:23:14 +0200

    GoTo + Anchor Event (#592)
    * Fixed Dependecies
    
    I moved some scenes in the last minute. Stupid me. I hope it is fixed now.
    
    * Revert "Fixed Dependecies"
    
    This reverts commit 9e7bcd8dbb29b69aa6373ad5553ebd686f437de6.
    
    * Auto stash before merge of "main" and "upstream/main"
    
    * Update Changelog
    
    To contain the changes I made during the last week.
    
    * Revert "Auto stash before merge of "main" and "upstream/main""
    
    This reverts commit 8e1699d08a204b65e3f292067b2c4e870aaee1c7.
    
    * Add the new styles to the remaining pickers
    
    TimelinePicker, ThemePicker, OperationPicker, GlossaryPicker. It also removes the [] around the items, except for the operation picker. I wasn't sure if it wouldn't make readability harder having three similar buttons next to each other, so I kept the [] there. 
    
    And it adds a "Bus:" label to the AudioPicker, because I thought it would make it cleaner.
    
    * Cleanup and Simplification of the MasterTree.gd
    
    * Auto stash before merge of "main" and "upstream/main"
    
    * Remove more Engine.get_singleton()'s
    
    :(
    
    * Make the DocsTreeHelper ignore the mouse
    
    It was blocking a small part of the MasterTree. This should fix #414
    
    * VoiceSupport Feature Fix: Make sure no default bus is needed
    
    There was a little annyoing warning popping up a lot.
    
    * Small Documentation fix
    
    * Add event documentation
    
    Adds a basic documentation of
    - Leave Event
    - Question Event
    - Choice Event
    - End Event
    - Condition Event
    - Set Value Event
    
    * Add the new documentations to the events
    
    You now have these little buttons on the right of the events that instantly lead to the documentation.
    
    * Test of Anchor and GoTo events
    
    * Check for complete dialogic_state information in start_from_save
    
    Apparently you can manage to save dialog_data that is empty. Maybe it has to do with the Autosave on timeline end? Need to investigate this. But this thing adds a check if the data is valid.
    
    * Changing the start_from_save function once again
    
    You now need to have "/" if you want to use the currently loaded state (mostly after importing). 
    Lot's of more checks added.
    
    * Fixing Timeline Editor merge issues
    
    * Small update fixes
    
    As always, the easiest way to handle merge conflicts for me is just to redo some parts of the code.
    
    * renaming the anchor to label, adding icons, updating timeline editor buttons
    
    Co-authored-by: Emilio Coppola <coppolaemilio@gmail.com>

--- addons/dialogic/Nodes/DialogNode.gd
+++ addons/dialogic/Nodes/DialogNode.gd
@@ -42,6 +42,7 @@ var characters
 
 # Parsing results
 var questions #for keeping track of the questions answered
+var anchors = {} # for listing the indexes of the anchors
 
 ### CURRENT STATE
 var current_timeline: String = ''
@@ -414,6 +415,7 @@ func load_dialog():
 		dialog_script = parse_characters(dialog_script)
 	dialog_script = parse_text_lines(dialog_script)
 	dialog_script = parse_branches(dialog_script)
+	parse_anchors()
 	return dialog_script
 
 # adds name coloring to the dialog texts
@@ -609,6 +611,13 @@ func _insert_glossary_definitions(text: String):
 		)
 	return final_text;
 
+func parse_anchors():
+	anchors = {}
+	var idx = 0
+	for event in dialog_script['events']:
+		if event['event_id'] == 'dialogic_015':
+			anchors[event['id']] = idx
+		idx += 1
 
 ## -----------------------------------------------------------------------------
 ## 					MAIN GAME-LOGIC 
@@ -883,6 +892,15 @@ func event_handler(event: Dictionary):
 			if event.get('set_random', false):
 				value = str(randi()%int(event.get("random_upper_limit", 100)-event.get('random_lower_limit', 0))+event.get('random_lower_limit', 0))
 			Dialogic.set_variable_from_id(event['definition'], value, operation)
+			_load_next_event()
+		# Anchor event
+		'dialogic_015':
+			emit_signal("event_start", "anchor", event)
+			_load_next_event()
+		# GoTo event
+		'dialogic_016':
+			emit_signal("event_start", "goto", event)
+			dialog_index = anchors[event.get('anchor_id')]
 			_load_next_event()
 		
 		
