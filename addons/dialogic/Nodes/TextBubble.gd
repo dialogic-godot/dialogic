b85eade57e7dd4613fd07e8d70f1c1e2e8efa953
KvaGram <0kvakk@gmailcom>
2021-11-06 01:30:34 +0100

    Implement fundamental text in commands
    Currently untested, you're welcome future me.
    
    Added a univerial regex that should fill all current and planned uses of commands in the text bubble.
    Rewrote most of update_text to use the new regex.
    Lots of comments for documentation
    Add func handle_event to handle events as they trigger

--- addons/dialogic/Nodes/TextBubble.gd
+++ addons/dialogic/Nodes/TextBubble.gd
@@ -3,8 +3,11 @@ extends Control
 
 var text_speed := 0.02 # Higher = lower speed
 var theme_text_speed = text_speed
-#proof of concept test
-var thing = -1
+
+#experimental database of current commands
+var commands = []
+#the regex matching object
+var regex = RegEx.new()
 
 onready var text_label = $RichTextLabel
 onready var name_label = $NameLabel
@@ -15,6 +18,7 @@ var _theme
 
 signal text_completed()
 signal letter_written()
+signal signal_request(arg)
 
 ## *****************************************************************************
 ##								PUBLIC METHODS
@@ -38,33 +42,37 @@ func update_name(name: String, color: Color = Color.white, autocolor: bool=false
 
 
 func update_text(text):
-	var regex = RegEx.new()
-	var result = null
-	# Removing commands from the text
-	#text = text.replace('[p]', '')
-	#edit proof of concept test
-	thing = text.find("[thing]")
-	text = text.replace("[thing]", "")
-	#end of edit
-	text = text.replace('[nw]', '')
-	if '[nw=' in text:
-		regex.compile("\\[nw=(.+?)\\](.*?)")
-		result = regex.search(text)
-		if result:
-			var wait_settings = result.get_string()
-			text = text.replace(wait_settings, '')
-			result = null
-	
-	# Speed
+	#regex moved from func scope to class scope
+	#regex compilation moved to _ready
+	#  - KvaGram
+	#var regex = RegEx.new()
+	var result:RegExMatch = null
 	text_speed = theme_text_speed # Resetting the speed to the default
-	# Regexing the speed tag
-	regex.compile("\\[speed=(.+?)\\](.*?)")
+	commands = []
+	
+	### remove commands from text, and store where and what they are
+	
+	#current regex: \[(nw|(nw|speed|signal|sound)=(.+?))\](.*?)
+	#remeber regex101.com is your friend. Do not shoot it.
+	#The capture groups
+	# 0 everything ex [speed=5]
+	# 1 the "nw" single command or one of the variable commands ex "nw" or "speed=5"
+	# 2 the command, assuming it is an variable command ex "speed"
+	# 3 the argument, again assuming a variable command ex "5"
+	# 4 nothing (ignore it)
+	#keep this up to date whenever the regex string is updated! - KvaGram
+	
 	result = regex.search(text)
-	if result:
-		var speed_settings = result.get_string()
-		var value = float(speed_settings.split('=')[1]) * 0.01
-		text_speed = value
-		text = text.replace(speed_settings, '')
+	#loops until all commands are cleared from the text
+	while result:
+		if result.get_string(1) == "nw" || result.get_string(2) == "nw":
+			#The no wait command is handled elsewhere. Ignore it.
+			pass
+		else:
+			#Store an assigned varible command as an array by 0 index in text, 1 command-name, 2 argument
+			commands.append([result.get_start(0), result.get_string(2), result.get_string(3)])
+		text = text.replace(result.get_string(0), "")
+		result = regex.search(text)
 	
 	# Updating the text and starting the animation from 0
 	text_label.bbcode_text = text
@@ -74,6 +82,17 @@ func update_text(text):
 	start_text_timer()
 	return true
 
+#handle an activated command.
+func handle_command(command:Array):
+	if(command[1] == "speed"):
+		text_speed = float(command[2]) * 0.01
+		# then restart the text timer?
+	elif(command[1] == "signal"):
+		emit_signal("signal_request", command[2])
+	elif(command[1] == "sound"):
+		#TODO: add a sound player to text bubble and a ready list of sounds to play
+		# a hardcoded sound will do for tests
+		pass
 
 func skip():
 	text_label.visible_characters = -1
@@ -193,10 +212,8 @@ func _on_writing_timer_timeout():
 	if get_parent().has_node('fade_in_tween_show_time') == false:
 		if _finished == false:
 			text_label.visible_characters += 1
-			#edit proof of concept test
-			if thing == text_label.visible_characters:
-				print("Hello thing")
-			#end of edit
+			if(commands.size()>0 && commands[0][0] <= text_label.visible_characters):
+				handle_command(commands.pop_front()) #handles the command, and removes it from the queue
 			if text_label.visible_characters > text_label.get_total_character_count():
 				_handle_text_completed()
 			elif (
@@ -244,4 +261,5 @@ func _ready():
 	reset()
 	$WritingTimer.connect("timeout", self, "_on_writing_timer_timeout")
 	text_label.meta_underlined = false
+	regex.compile("\\[(nw|(nw|speed|signal|sound)=(.+?))\\](.*?)")
 
