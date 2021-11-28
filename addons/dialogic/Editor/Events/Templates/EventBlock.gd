e7a875c214135db1e09113cb3196058c8541b6c7
Emilio Coppola <coppolaemilio@gmail.com>
2021-11-28 20:41:18 +0100

    making the lines thinner

--- addons/dialogic/Editor/Events/Templates/EventBlock.gd
+++ addons/dialogic/Editor/Events/Templates/EventBlock.gd
@@ -267,5 +267,5 @@ func _on_HelpButton_pressed():
 func _draw():
 #	draw_rect(rect: Rect2, color: Color, filled: bool = true, width: float = 1.0, antialiased: bool = false)
 	if current_indent_size > 0:
-		draw_rect(Rect2(Vector2(30 + ((indent_size + 2.2) * current_indent_size), 20), Vector2(2, rect_size.y + 10)), Color("#4D4D4D"), true)
-	draw_rect(Rect2(Vector2(30, 20), Vector2(2, rect_size.y + 10)), Color("#4D4D4D"), true)
+		draw_rect(Rect2(Vector2(30 + ((indent_size + 2.2) * current_indent_size), 20), Vector2(1, rect_size.y + 10)), Color("#4D4D4D"), true)
+	draw_rect(Rect2(Vector2(30, 20), Vector2(1, rect_size.y + 10)), Color("#4D4D4D"), true)
