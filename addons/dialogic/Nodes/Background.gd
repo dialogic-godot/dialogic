89581042fdc7908586715021587ca5b5f15975b7
Jowan-Spooner <42868150+Jowan-Spooner@users.noreply.github.com>
2021-11-30 18:57:29 +0100

    Add a non-stretching background image mode (#656)

--- addons/dialogic/Nodes/Background.gd
+++ addons/dialogic/Nodes/Background.gd
@@ -8,7 +8,10 @@ func _ready():
 	name = 'Background'
 	anchor_right = 1
 	anchor_bottom = 1
-	stretch_mode = TextureRect.STRETCH_SCALE
+	if DialogicResources.get_settings_value('dialog', 'stretch_backgrounds', true):
+		stretch_mode = TextureRect.STRETCH_SCALE
+	else:
+		stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_COVERED
 	show_behind_parent = true
 	mouse_filter = Control.MOUSE_FILTER_IGNORE
 
