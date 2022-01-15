5d0ee544654574f7c8bf296a689379f43ece4c14
zaknafean <zaknafean@gmail.com>
2022-01-15 10:34:30 -0500

    Visual Tweaks: History Row
    Fixed mis-sized'audio playback' button
    Changed default placeholder icon from character to text icon
    Removed obsolete code

--- addons/dialogic/Example Assets/History/HistoryRow.gd
+++ addons/dialogic/Example Assets/History/HistoryRow.gd
@@ -23,8 +23,6 @@ func _ready():
 	
 	assert(TextLabel is RichTextLabel, 'Text_Label must be a rich text label.')
 	assert(AudioButton is Button, 'Audio_Button must be a button.')
-	
-	#TextLabel.connect('minimum_size_changed', self, '_on_RichTextLabel_minimum_size_changed')
 
 
 func add_history(historyString, newAudio=''):
@@ -32,9 +30,11 @@ func add_history(historyString, newAudio=''):
 	audioPath = newAudio
 	if newAudio != '':
 		AudioButton.disabled = false
+		AudioButton.icon = load("res://addons/dialogic/Images/Event Icons/Main Icons/audio-event.svg")
+		AudioButton.flat = false
 	else:
 		AudioButton.disabled = true
-		AudioButton.icon = load("res://addons/dialogic/Images/Event Icons/character.svg")
+		AudioButton.icon = load("res://addons/dialogic/Images/Event Icons/Main Icons/text-event.svg")
 
 
 # Makes the row to look like the dialog box associated with the Theme
@@ -44,20 +44,16 @@ func load_theme(theme: ConfigFile):
 	TextLabel.set('custom_fonts/normal_font', theme_font)
 	TextLabel.set('custom_fonts/bold_font', DialogicUtil.path_fixer_load(theme.get_value('text', 'bold_font', 'res://addons/dialogic/Example Assets/Fonts/DefaultBoldFont.tres')))
 	TextLabel.set('custom_fonts/italics_font', DialogicUtil.path_fixer_load(theme.get_value('text', 'italic_font', 'res://addons/dialogic/Example Assets/Fonts/DefaultItalicFont.tres')))
-	#name_label.set('custom_fonts/font', DialogicUtil.path_fixer_load(theme.get_value('name', 'font', 'res://addons/dialogic/Example Assets/Fonts/NameFont.tres')))
-	
 	
 	var text_color = Color(theme.get_value('text', 'color', '#ffffffff'))
 	TextLabel.set('custom_colors/default_color', text_color)
-	#name_label.set('custom_colors/font_color', text_color)
-
+	
 	TextLabel.set('custom_colors/font_color_shadow', Color('#00ffffff'))
-	#name_label.set('custom_colors/font_color_shadow', Color('#00ffffff'))
-
+	
 	if theme.get_value('text', 'shadow', false):
 		var text_shadow_color = Color(theme.get_value('text', 'shadow_color', '#9e000000'))
 		TextLabel.set('custom_colors/font_color_shadow', text_shadow_color)
-
+	
 	var shadow_offset = theme.get_value('text', 'shadow_offset', Vector2(2,2))
 	TextLabel.set('custom_constants/shadow_offset_x', shadow_offset.x)
 	TextLabel.set('custom_constants/shadow_offset_y', shadow_offset.y)
@@ -68,20 +64,17 @@ func load_theme(theme: ConfigFile):
 	TextLabel.set('margin_right', text_margin.x * -1)
 	TextLabel.set('margin_top', text_margin.y)
 	TextLabel.set('margin_bottom', text_margin.y * -1)
-
+	
 	# Backgrounds
 	$TextureRect.texture = DialogicUtil.path_fixer_load(theme.get_value('background','image', "res://addons/dialogic/Example Assets/backgrounds/background-2.png"))
 	$TextureRect.expand = true
 	$ColorRect.color = Color(theme.get_value('background','color', "#ff000000"))
-
+	
 	if theme.get_value('background', 'modulation', false):
 		$TextureRect.modulate = Color(theme.get_value('background', 'modulation_color', '#ffffffff'))
 	else:
 		$TextureRect.modulate = Color('#ffffffff')
-
+	
 	$ColorRect.visible = theme.get_value('background', 'use_color', false)
 	$TextureRect.visible = theme.get_value('background', 'use_image', true)
 
-
-func _on_RichTextLabel_minimum_size_changed():
-	rect_min_size.y = TextLabel.rect_size.y
