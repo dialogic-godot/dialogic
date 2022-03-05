6e5f558833cccaa1cde620329907dd4fb06a66d4
zaknafean <zaknafean@gmail.com>
2022-03-05 14:32:16 -0500

    History Default theme adjustments
    HistoryRow now use the default theme
    HistoryRow themeing can now be disabled
    Default Background now matches default theme

--- addons/dialogic/Example Assets/History/HistoryRow.gd
+++ addons/dialogic/Example Assets/History/HistoryRow.gd
@@ -38,44 +38,56 @@ func add_history(historyString, newAudio=''):
 	AudioButton.focus_mode = FOCUS_NONE
 
 
-# Uses the default theme to make the font and boxes looks a bit nicer
+# Load Theme is called by 
 func load_theme(theme: ConfigFile):
 	# Text
 	var theme_font = DialogicUtil.path_fixer_load(theme.get_value('text', 'font', 'res://addons/dialogic/Example Assets/Fonts/DefaultFont.tres'))
 	TextLabel.set('custom_fonts/normal_font', theme_font)
 	TextLabel.set('custom_fonts/bold_font', DialogicUtil.path_fixer_load(theme.get_value('text', 'bold_font', 'res://addons/dialogic/Example Assets/Fonts/DefaultBoldFont.tres')))
 	TextLabel.set('custom_fonts/italics_font', DialogicUtil.path_fixer_load(theme.get_value('text', 'italic_font', 'res://addons/dialogic/Example Assets/Fonts/DefaultItalicFont.tres')))
+	#name_label.set('custom_fonts/font', DialogicUtil.path_fixer_load(theme.get_value('name', 'font', 'res://addons/dialogic/Example Assets/Fonts/NameFont.tres')))
+	
+	# setting the vertical alignment
+	var alignment = theme.get_value('text', 'alignment',0)
+	if alignment <= 2: # top
+		$HBoxContainer.alignment = BoxContainer.ALIGN_BEGIN
+	elif alignment <= 5: # center
+		$HBoxContainer.alignment = BoxContainer.ALIGN_CENTER
+	elif alignment <= 8: # bottom
+		$HBoxContainer.alignment = BoxContainer.ALIGN_END
 	
 	var text_color = Color(theme.get_value('text', 'color', '#ffffffff'))
 	TextLabel.set('custom_colors/default_color', text_color)
-	
+	#name_label.set('custom_colors/font_color', text_color)
+
 	TextLabel.set('custom_colors/font_color_shadow', Color('#00ffffff'))
-	
+	#name_label.set('custom_colors/font_color_shadow', Color('#00ffffff'))
+
 	if theme.get_value('text', 'shadow', false):
 		var text_shadow_color = Color(theme.get_value('text', 'shadow_color', '#9e000000'))
 		TextLabel.set('custom_colors/font_color_shadow', text_shadow_color)
-	
+
 	var shadow_offset = theme.get_value('text', 'shadow_offset', Vector2(2,2))
 	TextLabel.set('custom_constants/shadow_offset_x', shadow_offset.x)
 	TextLabel.set('custom_constants/shadow_offset_y', shadow_offset.y)
-	
+
 	# Margin
 	var text_margin = theme.get_value('text', 'margin', Vector2(20, 10))
-	TextLabel.set('margin_left', text_margin.x)
-	TextLabel.set('margin_right', text_margin.x * -1)
-	TextLabel.set('margin_top', text_margin.y)
-	TextLabel.set('margin_bottom', text_margin.y * -1)
-	
+	$HBoxContainer.set('margin_left', text_margin.x)
+	$HBoxContainer.set('margin_right', text_margin.x * -1)
+	$HBoxContainer.set('margin_top', text_margin.y)
+	$HBoxContainer.set('margin_bottom', text_margin.y * -1)
+
 	# Backgrounds
 	$TextureRect.texture = DialogicUtil.path_fixer_load(theme.get_value('background','image', "res://addons/dialogic/Example Assets/backgrounds/background-2.png"))
-	$TextureRect.expand = true
 	$ColorRect.color = Color(theme.get_value('background','color', "#ff000000"))
-	
+
 	if theme.get_value('background', 'modulation', false):
 		$TextureRect.modulate = Color(theme.get_value('background', 'modulation_color', '#ffffffff'))
 	else:
 		$TextureRect.modulate = Color('#ffffffff')
-	
+
 	$ColorRect.visible = theme.get_value('background', 'use_color', false)
 	$TextureRect.visible = theme.get_value('background', 'use_image', true)
-
+	
+	
