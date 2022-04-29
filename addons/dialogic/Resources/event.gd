529b324253b0626c7c719cdaa22b47ff9b1364c6
Emilio Coppola <coppolaemilio@protonmail.com>
2022-04-29 17:00:51 +0200

    Adding Dex's event shenanigans

--- addons/dialogic/Resources/event.gd
+++ addons/dialogic/Resources/event.gd
@@ -23,4 +23,78 @@ export (bool) var display_name : bool = true
 
 export (int) var sorting_index : int
 
+# -----------------------------------------
+# Emilio:
+# Stuff I yet don't understand made by Dex:
+# -----------------------------------------
 
+# This file is part of EventSystem, distributed under MIT license
+# and modified to work with Dialogic.
+# You can see the license of this file here
+# https://github.com/AnidemDex/Godot-EventSystem/blob/main/LICENSE
+
+
+## 
+## Base class for all events.
+##
+## @desc: 
+##    Every event relies on this class. 
+##    If you want to do your own event, you should [code]extend[/code] this class.
+##
+
+## Emmited when the event starts.
+## The signal is emmited with the event resource [code]event_resource[/code]
+signal event_started(event_resource)
+
+## Emmited when the event finish. 
+## The signal is emmited with the event resource [code]event_resource[/code]
+signal event_finished(event_resource)
+
+##########
+# Default Event Properties
+##########
+
+## Determines if the event will go to next event inmediatly or not. 
+## If value is true, the next event will be executed when event ends.
+export(bool) var continue_at_end:bool = true setget _set_continue
+
+
+## Executes the event behaviour.
+func execute() -> void:
+	emit_signal("event_started", self)
+	
+	call_deferred("_execute")
+
+
+## Ends the event behaviour.
+func finish() -> void:
+	emit_signal("event_finished", self)
+
+
+func _execute() -> void:
+	finish()
+
+
+func _set_continue(value:bool) -> void:
+	continue_at_end = value
+	property_list_changed_notify()
+	emit_changed()
+
+
+func property_can_revert(property:String) -> bool:
+	if property == "event_node_path":
+		return true
+	return false
+
+
+func property_get_revert(property:String):
+	if property == "event_node_path":
+		return NodePath()
+
+
+func _to_string() -> String:
+	return "[{name}:{id}]".format({"name":name, "id":get_instance_id()})
+
+
+func _hide_script_from_inspector():
+	return true
