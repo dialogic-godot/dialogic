d8657a679ba36f9161e35cebc393f4944e6e16c6
Jowan-Spooner <raban-loeffler@posteo.de>
2022-06-13 20:43:33 +0200

    Portrait management, Character event improvement
    # Portrait display nodes
    There are now 2 more Dialogic Display Nodes:
    
    - PortraitPosition scene is a Position2D node that only has a portrait_index (any integer  0 <= x <= 100) 
    - PortraitHolder scene is a CanvasLayer that the portraits will be added to as children.
    
    There should always only be at most one Portrait Holder (although more will not break) and there can be unlimited PortraitPositions, although they should ideally have different indexes. The DefaultDialogNode has 5 positions set up with the indexes 1 to 5
    
    # DialogicGameHandler functions
    - update_portrait() -> updates or adds a portrait
    - remove_portrait() -> removes a character
    - is_character_joined() -> true if the character has a visible portrait
    - get_current_portrait_info_of_character() -> returns all the stored info on the portrait of the character (e.g portrait name, position, the actual node, etc.)
    
    These functions mainly use the `current_portraits` array to communicate with each other/store stuff.
    
    # Event updates
    The character event and text event have been updated to use the methods listed above to manipulate the portraits. 
    
    - Character Event has a position spin box. This is not optimal, especiall as writing a number in the text format is really obnoxious. My suggestion are somewhat named positions, either in the settings (e.g {1:'left', 2:'center_left', 3:'center', 4:'center_right', 5:'right'}) or as strings actually entered on the PortraitPosition nodes (then it would be hard to suggest them tho).
    
    
    # Example Assets
    I've added CC0 portraits of characters to test this. If this is to much, we can delete them again. They are the same that I used in the official Dialogic Test Project. 
    
    # Missing from last commit :(
    The character resource actually has a couple of needed updates for the last commit that I forgot to stage. These include new variables for nicknames, description, scale and theme.

---
+++ addons/dialogic/Display/PortraitHolder.gd
@@ -0,0 +1,4 @@
+extends CanvasLayer
+
+func _ready():
+	add_to_group('dialogic_portrait_holder')
--- addons/dialogic/Other/DialogicGameHandler.gd
+++ addons/dialogic/Other/DialogicGameHandler.gd
@@ -8,7 +8,7 @@ var current_timeline_events = []
 
 var current_state = null setget set_current_state
 var current_event_idx = 0
-var current_portraits = {}
+var current_portraits = []
 var current_bg_music
 var current_bg_image
 var current_name
@@ -21,7 +21,7 @@ signal timeline_ended()
 ## 						INPUT (WIP)
 ################################################################################
 # This shouldn't be handled by this script I think, but for testing purposes this works.
-func _input(event):
+func _input(event:InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed:
 		if current_state == states.IDLE:
 			handle_next_event()
@@ -29,7 +29,7 @@ func _input(event):
 ################################################################################
 ## 						TIMELINE+EVENT HANDLING
 ################################################################################
-func start_timeline(timeline_resource):
+func start_timeline(timeline_resource) -> void:
 	# load the resource if only the path is given
 	if typeof(timeline_resource) == TYPE_STRING:
 		timeline_resource = load(timeline_resource)
@@ -42,11 +42,11 @@ func start_timeline(timeline_resource):
 	handle_next_event()
 
 
-func handle_next_event(ignore_argument = ""):
+func handle_next_event(ignore_argument = "") -> void:
 	handle_event(current_event_idx+1)
 	
 	
-func handle_event(event_index):
+func handle_event(event_index:int) -> void:
 	hide_all_choices()
 	if event_index >= len(current_timeline_events):
 		emit_signal('timeline_ended')
@@ -54,45 +54,87 @@ func handle_event(event_index):
 	
 	current_event_idx = event_index
 	var event:DialogicEvent = current_timeline_events[event_index]
-	print("[D] Handle Event ", event_index, ": ", event)
+	print("\n[D] Handle Event ", event_index, ": ", event)
 	if event.continue_at_end:
-		print("    -> AUTO CONTINUE!")
+		print("    -> WILL AUTO CONTINUE!")
 		event.connect("event_finished", self, 'handle_next_event')
 	event.execute(self)
 
 ################################################################################
 ## 						DISPLAY NODES
 ################################################################################
-func _ready():
+func _ready() -> void:
 	reset_all_display_nodes()
 
-func reset_all_display_nodes():
+func reset_all_display_nodes() -> void:
 	update_dialog_text('')
 	update_name_label('', Color.white)
-	
+	hide_all_choices()
 
-func update_dialog_text(text):
+func update_dialog_text(text:String) -> void:
 	for text_node in get_tree().get_nodes_in_group('dialogic_dialog_text'):
 		text_node.bbcode_text = text
 		text_node.reveal_text()
 
-func update_portrait(character, portrait, position, z_index, move, animation):
-	pass
-
-func update_name_label(name, color = Color(), font = null):
+func update_name_label(name:String, color:Color = Color()) -> void:
 	for name_label in get_tree().get_nodes_in_group('dialogic_name_label'):
 		name_label.text = name
 		name_label.self_modulate = color
-		if font:
-			name_label.font = font
 
-func hide_all_choices():
+func update_portrait(character: DialogicCharacter, portrait:String = "", position_idx:int = -1, z_index:int = 0, move:bool = false, animation:String = "") -> void:
+	print("HELLO GUYS")
+	
+	if not character:
+		return
+	if len(get_tree().get_nodes_in_group('dialogic_portrait_holder')) == 0:
+		assert('[Dialogic] If you want to display portraits, you need a PortraitHolder scene!')
+		
+	if is_character_joined(character):
+		var info = get_current_portrait_info_of_character(character)
+		if position_idx == -1:
+			position_idx = info.position
+		if portrait == "":
+			portrait = info.portrait
+	
+	print(character.name, portrait, position_idx)
+	if portrait and position_idx:
+		for node in get_tree().get_nodes_in_group('dialogic_portrait_position'):
+			if node.position_index == position_idx:
+				print("hurray")
+				var path = character.portraits[portrait].path
+				for portrait in current_portraits:
+					if portrait.character == character:
+						portrait.node.queue_free()
+						current_portraits.erase(portrait)
+				if not path.ends_with('.tscn'):
+					var sprite = Sprite.new()
+					get_tree().get_nodes_in_group('dialogic_portrait_holder')[0].add_child(sprite)
+					sprite.texture = load(path)
+					sprite.centered = false
+					sprite.scale = Vector2(1,1)*character.portraits[portrait].scale
+					sprite.global_position = node.global_position + character.portraits[portrait].offset
+					sprite.global_position.x -= sprite.texture.get_width()/2.0*character.portraits[portrait].scale*character.scale
+					sprite.global_position.y -= sprite.texture.get_height()*character.portraits[portrait].scale*character.scale
+					current_portraits.append({'character':character, 'portrait':portrait, 'position':position_idx, 'node':sprite})
+				
+	else:
+		for portrait in current_portraits:
+			if portrait.character == character:
+				portrait.node.queue_free()
+				current_portraits.erase(portrait)
+
+func remove_portrait(character:DialogicCharacter) -> void:
+	var info = get_current_portrait_info_of_character(character)
+	info.node.queue_free()
+	current_portraits.erase(info)
+
+func hide_all_choices() -> void:
 	for node in get_tree().get_nodes_in_group('dialogic_choice_button'):
 		node.hide()
 		if node.is_connected('pressed', self, 'choice_selected'):
 			node.disconnect('pressed', self, 'choice_selected')
 
-func show_current_choices():
+func show_current_choices() -> void:
 	hide_all_choices()
 	var button_idx = 1
 	for choice_index in get_current_choice_indexes():
@@ -100,14 +142,14 @@ func show_current_choices():
 		show_choice(button_idx, choice_event.Text, true, choice_index)
 		button_idx += 1
 
-func show_choice(button_index, text, enabled, event_index):
+func show_choice(button_index:int, text:String, enabled:bool, event_index:int) -> void:
 	for node in get_tree().get_nodes_in_group('dialogic_choice_button'):
 		if node.choice_index == button_index:
 			node.show()
 			node.text = text
 			node.connect('pressed', self, 'choice_selected', [event_index])
 
-func choice_selected(event_index):
+func choice_selected(event_index:int) -> void:
 	hide_all_choices()
 	current_state = states.IDLE
 	handle_event(event_index)
@@ -116,11 +158,11 @@ func choice_selected(event_index):
 ################################################################################
 ## 						HELPERS
 ################################################################################
-func set_current_state(new_state):
+func set_current_state(new_state:int) -> void:
 	current_state = new_state
 	emit_signal('state_changed', new_state)
 
-func is_question(index):
+func is_question(index:int) -> bool:
 	if current_timeline_events[index] is DialogicTextEvent:
 		if len(current_timeline_events)-1 != index:
 			if current_timeline_events[index+1] is DialogicChoiceEvent:
@@ -150,3 +192,16 @@ func get_current_choice_indexes() -> Array:
 			ignore -= 1
 	
 	return choices
+
+func is_character_joined(character:DialogicCharacter) -> bool:
+	for portrait in current_portraits:
+		if portrait.character == character:
+			return true
+	return false
+
+func get_current_portrait_info_of_character(character:DialogicCharacter) -> Array:
+	for portrait in current_portraits:
+		if portrait.character == character:
+			return portrait
+	return []
+
