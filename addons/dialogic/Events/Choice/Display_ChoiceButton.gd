9e1345e7922a9ca61c83b6155b642767cbe556f4
M7mdKady14 <104588917+M7mdKady14@users.noreply.github.com>
2022-07-14 16:20:48 +0200

    Add files via upload

--- addons/dialogic/Events/Choice/Display_ChoiceButton.gd
+++ addons/dialogic/Events/Choice/Display_ChoiceButton.gd
@@ -5,20 +5,9 @@ class_name DialogicDisplay_ChoiceButton, 'icon.png'
 export (int) var choice_index = -1
 
 export(AudioStream) var sound_pressed
-export(AudioStream) var sound_released
 export(AudioStream) var sound_hover
-export(AudioStream) var sound_unhover
 export(AudioStream) var sound_focus
-export(AudioStream) var sound_unfocus
-
-onready var sound_node = get_parent().get_node("DialogicDisplay_ButtonSound")
 
 func _ready():
-	connect('pressed', sound_node, '_on_pressed', [sound_pressed])
-	connect('button_up', sound_node, '_on_released', [sound_released])
-	connect('mouse_entered', sound_node, '_on_hover', [sound_hover])
-	connect('mouse_exited', sound_node, '_on_unhover', [sound_unhover])
-	connect('focus_entered', sound_node, '_on_focus', [sound_focus])
-	connect('focus_exited', sound_node, '_on_unfocus', [sound_unfocus])
 	add_to_group('dialogic_choice_button')
 	hide()
--- addons/dialogic/Events/Choice/SoundButton.gd
+++ addons/dialogic/Events/Choice/SoundButton.gd
@@ -4,20 +4,26 @@ class_name DialogicDisplay_ButtonSound
 
 #all the sounds
 export(AudioStream) var sound_pressed
-export(AudioStream) var sound_released
 export(AudioStream) var sound_hover
-export(AudioStream) var sound_unhover
 export(AudioStream) var sound_focus
-export(AudioStream) var sound_unfocus
 
 func _ready():
 	add_to_group('dialogic_button_sound')
+	_connect_all_buttons()
 
 #basic play sound
 func play_sound(sound) -> void:
 	if sound != null:
 		stream = sound
 		play()
+
+func _connect_all_buttons():
+	for child in get_parent().get_children():
+		if child is DialogicDisplay_ChoiceButton:
+			child.connect('pressed', self, '_on_pressed', [child.sound_pressed])
+			child.connect('mouse_entered', self, '_on_hover', [child.sound_hover])
+			child.connect('focus_entered', self, '_on_focus', [child.sound_focus])
+
 
 #the custom_sound argument comes from the specifec button and get used
 #if none are found, it uses the above sounds
@@ -28,33 +34,15 @@ func _on_pressed(custom_sound) -> void:
 	else:
 		play_sound(sound_pressed)
 
-func _on_released(custom_sound) -> void:
-	if custom_sound != null:
-		play_sound(custom_sound)
-	else:
-		play_sound(sound_released)
-
 func _on_hover(custom_sound) -> void:
 	if custom_sound != null:
 		play_sound(custom_sound)
 	else:
 		play_sound(sound_hover)
 
-func _on_unhover(custom_sound) -> void:
-	if custom_sound != null:
-		play_sound(custom_sound)
-	else:
-		play_sound(sound_unhover)
-
 func _on_focus(custom_sound) -> void:
 	if custom_sound != null:
 		play_sound(custom_sound)
 	else:
 		play_sound(sound_focus)
-
-func _on_unfocus(custom_sound) -> void:
-	if custom_sound != null:
-		play_sound(custom_sound)
-	else:
-		play_sound(sound_unfocus)
 
