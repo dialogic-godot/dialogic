a6b759f19ac1a2b28d72228c5c71ceaaf45dae94
M7mdKady14 <104588917+M7mdKady14@users.noreply.github.com>
2022-07-15 13:07:23 +0200

    Add files via upload

--- addons/dialogic/Events/Call Node/event.gd
+++ addons/dialogic/Events/Call Node/event.gd
@@ -4,15 +4,20 @@ class_name DialogicCallNodeEvent
 
 # DEFINE ALL PROPERTIES OF THE EVENT
 var Path: String = ""
-var Method:String = ""
-var Arguments:Array=[]
+var Method: String = ""
+var Arguments: Array = []
+var Wait: bool = false
 
-var Wait:bool = false
+var Signal_Name: String
 var Inline: bool = false
-var Signal_Number: int = 1
+var One_Off: bool = true
+
 
 
 func _execute() -> void:
+	if Inline:
+		dialogic.connect('timeline_ended', self, '_disconnect_signal')
+	
 	if Path.begins_with('root'):
 		Path = "/"+Path
 	if not "/" in Path and dialogic.get_node('/root').has_node(Path):
@@ -30,14 +35,17 @@ func _execute() -> void:
 	
 	finish()
 
-func _call_on_signal(number):
+func _call_on_signal(arg):
 	
-	#	_call_on_signal()
-	if number != str(Signal_Number):
+	if arg != Signal_Name:
 		return
-	dialogic.disconnect("text_signal", self, "_call_on_signal")
+	if One_Off:
+		dialogic.disconnect("text_signal", self, "_call_on_signal")
 	var n = dialogic.get_node_or_null(Path)
 	n.callv(Method, Arguments)
+
+func _disconnect_signal():
+	dialogic.disconnect("text_signal", self, "_call_on_signal")
 
 ################################################################################
 ## 						INITIALIZE
@@ -65,7 +73,8 @@ func get_shortcode_parameters() -> Dictionary:
 		"method"	: "Method",
 		"args"		: "Arguments",
 		"wait"		: "Wait",
-		"inline"	: "Inline"
+		"inline"	: "Inline",
+		"signal_name": "Signal_Name",
 	}
 
 
@@ -78,6 +87,7 @@ func build_event_editor():
 	add_body_edit('Method', ValueType.SinglelineText, 'Method:')
 	add_body_edit('Wait', ValueType.Bool, 'Wait:')
 	add_body_edit('Inline', ValueType.Bool, 'Inline:')
-	add_body_edit('Signal_Number', ValueType.Integer, 'Signal Number', '', {}, 'Inline == true')
+	add_body_edit('Signal_Name', ValueType.SinglelineText, 'Signal Name', '', {}, 'Inline == true')
+	add_body_edit('One_Off', ValueType.Bool, 'One Off', '', {}, 'Inline == true')
 	add_body_line_break()
 	add_body_edit('Arguments', ValueType.StringArray, 'Arguments:')
