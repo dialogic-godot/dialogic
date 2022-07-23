d85a620a233d5dd2418bbcec881102ed02d826d3
M7mdKady14 <104588917+M7mdKady14@users.noreply.github.com>
2022-07-23 14:55:47 +0200

    2.X Add "inline mode" to Call Node event (#1022)
    Allows to set a call node to call the method once a signal has been emitted from a text event with a specific argument.

--- addons/dialogic/Events/Call Node/event.gd
+++ addons/dialogic/Events/Call Node/event.gd
@@ -4,12 +4,20 @@ class_name DialogicCallNodeEvent
 
 # DEFINE ALL PROPERTIES OF THE EVENT
 var Path: String = ""
-var Method:String = ""
-var Arguments:Array=[]
+var Method: String = ""
+var Arguments: Array = []
+var Wait: bool = false
 
-var Wait:bool = false
+var Signal_Name: String
+var Inline: bool = false
+var Single_Use: bool = false
+
+
 
 func _execute() -> void:
+	if Inline:
+		dialogic.connect('timeline_ended', self, '_disconnect_signal')
+	
 	if Path.begins_with('root'):
 		Path = "/"+Path
 	if not "/" in Path and dialogic.get_node('/root').has_node(Path):
@@ -18,12 +26,27 @@ func _execute() -> void:
 	var n = dialogic.get_node_or_null(Path)
 	if n:
 		if n.has_method(Method):
-			if Wait:
+			if Inline:
+				dialogic.connect("text_signal", self, "_call_on_signal", [], CONNECT_PERSIST)
+			elif Wait:
 				yield(n.callv(Method, Arguments), "completed")
 			else:
 				n.callv(Method, Arguments)
 	
 	finish()
+
+func _call_on_signal(arg):
+	
+	if arg != Signal_Name:
+		return
+	if Single_Use:
+		dialogic.disconnect("text_signal", self, "_call_on_signal")
+	var n = dialogic.get_node_or_null(Path)
+	n.callv(Method, Arguments)
+
+func _disconnect_signal():
+	if dialogic.is_connected('text_signal', self, '_call_on_signal'):
+		dialogic.disconnect("text_signal", self, "_call_on_signal")
 
 ################################################################################
 ## 						INITIALIZE
@@ -50,7 +73,10 @@ func get_shortcode_parameters() -> Dictionary:
 		"path"		: "Path",
 		"method"	: "Method",
 		"args"		: "Arguments",
-		"wait"		: "Wait"
+		"wait"		: "Wait",
+		"inline"	: "Inline",
+		"signal": "Signal_Name",
+		"single_use": "Single_Use",
 	}
 
 
@@ -62,5 +88,8 @@ func build_event_editor():
 	add_header_edit('Path', ValueType.SinglelineText, 'Path:')
 	add_body_edit('Method', ValueType.SinglelineText, 'Method:')
 	add_body_edit('Wait', ValueType.Bool, 'Wait:')
+	add_body_edit('Inline', ValueType.Bool, 'Inline:')
+	add_body_edit('Signal_Name', ValueType.SinglelineText, 'Signal Name', '', {}, 'Inline == true')
+	add_body_edit('Single_Use', ValueType.Bool, 'Single Use', '', {}, 'Inline == true')
 	add_body_line_break()
 	add_body_edit('Arguments', ValueType.StringArray, 'Arguments:')
