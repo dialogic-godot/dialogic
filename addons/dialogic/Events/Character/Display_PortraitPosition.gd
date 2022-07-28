9b20a138a83f11ddddd32aa7250ac1a913b5e89f
Emilio Coppola <coppolaemilio@protonmail.com>
2022-07-28 03:45:27 +0200

    now for real tho

--- addons/dialogic/Events/Character/Display_PortraitPosition.gd
+++ addons/dialogic/Events/Character/Display_PortraitPosition.gd
@@ -2,7 +2,7 @@ extends Position2D
 
 class_name DialogicDisplay_PortraitPosition
 
-export (int, 0, 100) var position_index = 0
+@export var position_index = 0
 
 func _ready() -> void:
 	add_to_group('dialogic_portrait_position')
