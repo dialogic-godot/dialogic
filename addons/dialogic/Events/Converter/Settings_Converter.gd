fd93c347487ce1c5a7b92351381228dfd9cdda7c
thebardsrc <thebardsrc@gmail.com>
2022-07-31 12:03:42 -0600

    finish moving to the settings window. starting actual timeline conversion

--- addons/dialogic/Events/Converter/Settings_Converter.gd
+++ addons/dialogic/Events/Converter/Settings_Converter.gd
@@ -15,10 +15,13 @@ var contents
 
 var conversionReady = false
 
+var varSubsystemInstalled = false
+
 func refresh():
 	pass
 
 func _on_verify_pressed():
+	
 	var file = File.new()
 	
 	%OutputLog.text = ""
@@ -144,6 +147,12 @@ func _on_verify_pressed():
 			%OutputLog.text += "Theme files found: [color=red]" + str(themeDirectory.size()) + "[/color]\r\n"
 			%OutputLog.text += "[color=yellow]There may be an issue, please check in Dialogic 1.x to make sure that is correct![/color]\r\n"
 			
+		# dirty check for the variable subsystem, as properly calling has subsystem is complicated currently
+		varSubsystemInstalled = file.file_exists("res://addons/dialogic/Events/Variable/event.gd")
+		
+		if !varSubsystemInstalled:
+			%OutputLog.text += "[color=yellow]Variable subsystem is not present in this Dialogic! Variables will not be converted![/color]"
+			
 		%OutputLog.text += "\r\n"
 		
 		%OutputLog.text += "Initial integrity check completed!\r\n"
@@ -203,3 +212,198 @@ func recursive_search(currentCheck, currentDictionary, currentFolder):
 
 
 
+
+
+func _on_begin_pressed():
+	%OutputLog.text += "-----------------------------------------\r\n"
+	%OutputLog.text += "Beginning file conversion:\r\n"
+	%OutputLog.text += "\r\n"
+	
+	convertTimelines()
+	convertCharacters()
+	convertVariables()
+	convertGlossaries()
+	convertThemes()
+	
+	%OutputLog.text += "All conversions complete!\r\n"
+	
+
+func convertTimelines():
+	%OutputLog.text += "Converting timelines: \r\n"
+	for item in timelineFolderBreakdown:
+		var folderPath = timelineFolderBreakdown[item]
+		%OutputLog.text += "Timeline " + folderPath + item +": "
+		var jsonData = {}
+		var file = File.new()
+		file.open("res://dialogic/timelines/" + item,File.READ)
+		var fileContent = file.get_as_text()
+		var json_object = JSON.new()
+		
+		var error = json_object.parse(fileContent)
+		
+		if error == OK:
+			contents = json_object.get_data()
+			var fileName = contents["metadata"]["name"]
+			%OutputLog.text += "Name: " + fileName + ", " + str(contents["events"].size()) + " timeline events"
+			
+			var directory = Directory.new()
+			var directoryCheck = directory.dir_exists(conversionRootFolder + "/timelines" + folderPath)
+			if !directoryCheck: 
+				directory.open(conversionRootFolder + "/timelines")
+				
+				var progresiveDirectory = ""
+				for pathItem in folderPath.split('/'):
+					directory.open(conversionRootFolder + "/timelines" + progresiveDirectory)
+					if pathItem!= "":
+						progresiveDirectory += "/" + pathItem
+					if !directory.dir_exists(conversionRootFolder + "/timelines" + progresiveDirectory):
+						directory.make_dir(conversionRootFolder + "/timelines" + progresiveDirectory)
+				
+				
+			file.open(conversionRootFolder + "/timelines" + folderPath + "/" + fileName + ".dtl",File.WRITE)
+			for event in contents["events"]:
+				var eventLine = ""
+				
+				if "dialogic_" in event["event_id"]:
+					match event["event_id"]:
+						
+					
+					
+					file.store_string(eventLine)
+				else: 
+					eventLine += "# Custom event: "
+					eventLine += str(event)
+					eventLine = eventLine.replace("{", "*")
+					eventLine = eventLine.replace("}", "*")
+					
+					file.store_string(eventLine)
+				
+				file.store_string("\r\n\r\n")
+			file.close()
+			
+			
+			
+			%OutputLog.text += "\r\n"
+		else:
+			%OutputLog.text += "[color=red]There was a problem parsing this file![/color]\r\n"
+		
+	%OutputLog.text += "\r\n"
+	
+	
+func convertCharacters(): 
+	%OutputLog.text += "Converting characters: \r\n"
+	for item in characterFolderBreakdown:
+		var folderPath = characterFolderBreakdown[item]
+		%OutputLog.text += "Character " + folderPath + item +": "
+		var jsonData = {}
+		var file = File.new()
+		file.open("res://dialogic/characters/" + item,File.READ)
+		var fileContent = file.get_as_text()
+		var json_object = JSON.new()
+		
+		var error = json_object.parse(fileContent)
+		
+		if error == OK:
+			contents = json_object.get_data()
+			var fileName = contents["name"]
+			%OutputLog.text += "Name: " + fileName
+			
+			if ("[" in fileName) || ("]" in fileName):
+				%OutputLog.text += " [color=yellow]Stripping brackets from name![/color]"
+				fileName = fileName.replace("[","")
+				fileName = fileName.replace("]","")
+				
+			
+			var directory = Directory.new()
+			var directoryCheck = directory.dir_exists(conversionRootFolder + "/characters" + folderPath)
+			if !directoryCheck: 
+				directory.open(conversionRootFolder + "/characters")
+				
+				var progresiveDirectory = ""
+				for pathItem in folderPath.split('/'):
+					directory.open(conversionRootFolder + "/characters" + progresiveDirectory)
+					if pathItem!= "":
+						progresiveDirectory += "/" + pathItem
+					if !directory.dir_exists(conversionRootFolder + "/characters" + progresiveDirectory):
+						directory.make_dir(conversionRootFolder + "/characters" + progresiveDirectory)
+			
+			# using the resource constructor for this one
+			
+			var current_character = DialogicCharacter.new()
+			current_character.resource_path = conversionRootFolder + "/characters" + folderPath + "/" + fileName + ".dch"
+			# Everything needs to be in exact order
+
+			current_character.color = Color.html(contents["color"])
+			var customInfoDict = {}
+			customInfoDict["sound_moods"] = {}
+			customInfoDict["theme"] = ""
+			current_character.custom_info = customInfoDict
+			current_character.description = contents["description"]
+			if contents["display_name"] == "":
+				current_character.display_name = contents["name"]
+			else:
+				current_character.display_name = contents["display_name"]
+			current_character.mirror = contents["mirror_portraits"]
+			current_character.name = contents["name"]
+			current_character.nicknames = []
+			current_character.offset = Vector2(0,0)
+			current_character.portraits = {}
+			current_character.scale = 1.0
+			
+			ResourceSaver.save(current_character.resource_path, current_character)	
+			#file.open(conversionRootFolder + "/characters" + folderPath + "/" + fileName + ".dch",File.WRITE)
+			#json_object = JSON.new()
+			#var output_string = json_object.stringify(charData, "\r")
+			#file.store_string(output_string)
+			
+			#file.close()
+			
+			
+			
+			%OutputLog.text += "\r\n"
+		else:
+			%OutputLog.text += "[color=red]There was a problem parsing this file![/color]\r\n"
+			
+	
+	%OutputLog.text += "\r\n"
+	
+
+func convertVariables():
+	%OutputLog.text += "Converting variables: \r\n"
+	
+	# Creating a file with a format identical to how the variables are stored in project settings
+	if varSubsystemInstalled:
+		var newVariableDictionary = {}
+		for varItem in definitionFolderBreakdown:
+			if definitionFolderBreakdown[varItem]["type"] == "variable":
+				if definitionFolderBreakdown[varItem]["path"] == "/":
+					newVariableDictionary[definitionFolderBreakdown[varItem]["name"]] = definitionFolderBreakdown[varItem]["value"]
+				else:
+					# I will fill this one in later, need to figure out the recursion for it
+					pass
+							
+							
+		
+		var file = File.new()
+		file.open(conversionRootFolder + "/variables.json",File.WRITE)
+		var json_object = JSON.new()
+		var output_string = json_object.stringify(newVariableDictionary, "\n")
+		file.store_string(output_string)
+		file.close()
+	else:
+		%OutputLog.text += "[color=yellow]Variable subsystem is not present! Variables were not converted![/color]\r\n"
+	
+	
+	
+	%OutputLog.text += "\r\n"
+	
+
+func convertGlossaries():
+	%OutputLog.text += "Converting glossaries: [color=red]not currently implemented[/color] \r\n"
+	
+	%OutputLog.text += "\r\n"
+
+func convertThemes():
+	%OutputLog.text += "Converting themes: [color=red]not currently implemented[/color] \r\n"
+	
+	%OutputLog.text += "\r\n"
