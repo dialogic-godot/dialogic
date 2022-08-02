ac521c8c7cd6db475ed41e5493f2aa6d5955b758
Jowan-Spooner <raban-loeffler@posteo.de>
2022-08-02 18:05:36 +0200

    Add default mood
    You can now select a default mood that will be used for all portraits that don't have a custom mood selected.

--- addons/dialogic/Events/Text/CharacterEdit_TypingSounds.gd
+++ addons/dialogic/Events/Text/CharacterEdit_TypingSounds.gd
@@ -6,12 +6,14 @@ signal changed
 func _ready():
 	find_parent('CharacterEditor').portrait_selected.connect(_on_portrait_selected)
 	%PortraitMood.get_suggestions_func = [self, 'mood_suggestions']
+	%DefaultMood.get_suggestions_func = [self, 'mood_suggestions']
 
 func load_character(character:DialogicCharacter):
 	for mood in %Moods.get_children():
 		mood.queue_free()
 	
 	%PortraitMood.set_value('')
+	%DefaultMood.set_value(character.custom_info.get('sound_moods_default', ''))
 	
 	for mood in character.custom_info.get('sound_moods', {}):
 		create_mood_item(character.custom_info.sound_moods[mood])
@@ -20,6 +22,7 @@ func save_character(character:DialogicCharacter):
 	var moods = {}
 	for node in %Moods.get_children():
 		moods[node.get_data().name] = node.get_data()
+	character.custom_info['sound_mood_default'] = %DefaultMood.current_value
 	character.custom_info['sound_moods'] = moods
 
 func _on_AddMood_pressed():
@@ -40,13 +43,13 @@ func duplicate_mood_item(item):
 func _on_portrait_selected(previous, current):
 	if current and is_instance_valid(current):
 		%PortraitMood.set_value(current.portrait_data.get('sound_mood', ''))
-		%PortraitMoodLabel.text = 'Typing-Sound-Mood for portrait "%s":'%current.get_portrait_name()
+		%PortraitMoodLabel.text = 'Mood for "%s":'%current.get_portrait_name()
 
 
 func mood_suggestions(filter):
 	var suggestions = {}
 	for child in %Moods.get_children():
-		if !filter or filter.to_lower() in child.get_data().name.to_lower():
+		if filter.is_empty() or filter.to_lower() in child.get_data().name.to_lower():
 			suggestions[child.get_data().name] = {'value':child.get_data().name}
 	return suggestions
 
@@ -54,4 +57,10 @@ func mood_suggestions(filter):
 func _on_PortraitMood_value_changed(property_name, value):
 	if find_parent('CharacterEditor').current_portrait:
 		find_parent('CharacterEditor').current_portrait.portrait_data['sound_mood'] = value
+	emit_signal("changed")
+
+
+func _on_default_mood_value_changed(property_name, value):
+	if find_parent('CharacterEditor').current_portrait:
+		find_parent('CharacterEditor').current_portrait.portrait_data['sound_mood_default'] = value
 	emit_signal("changed")
