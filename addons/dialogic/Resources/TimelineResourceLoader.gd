ac5d47a3fd77daf9d1d2e414a68c5362227a942f
Jowan-Spooner <42868150+Jowan-Spooner@users.noreply.github.com>
2022-08-06 02:27:09 +0200

    Add dependency updates when moving files in godot (#1085)
    Works for:
    - Character portraits paths
    - References to characters, timelines or strings that start with "res://" in all properties of events that are listed in the get_shortcode_parameters() method. This means that currently it does NOT work on the Text event, but that shouldn't be too much of a problem, because characters are only stored by reference anyways.

--- addons/dialogic/Resources/TimelineResourceLoader.gd
+++ addons/dialogic/Resources/TimelineResourceLoader.gd
@@ -94,3 +94,32 @@ func _load(path: String, original_path: String, use_sub_threads: bool, cache_mod
 	res._events = events
 	
 	return res
+
+
+func _get_dependencies(path:String, add_type:bool):
+	var depends_on : PackedStringArray
+	var timeline:DialogicTimeline = load(path)
+	for event in timeline._events:
+		for property in event.get_shortcode_parameters().values():
+			if event.get(property) is DialogicTimeline:
+				depends_on.append(event.get(property).resource_path)
+			elif event.get(property) is DialogicCharacter:
+				depends_on.append(event.get(property).resource_path)
+			elif typeof(event.get(property)) == TYPE_STRING and event.get(property).begins_with('res://'):
+				depends_on.append(event.get(property))
+	return depends_on
+
+func _rename_dependencies(path: String, renames: Dictionary):
+	var timeline:DialogicTimeline = load(path)
+	for event in timeline._events:
+		for property in event.get_shortcode_parameters().values():
+			if event.get(property) is DialogicTimeline:
+				if event.get(property).resource_path in renames:
+					event.set(property, load(renames[event.get(property).resource_path]))
+			elif event.get(property) is DialogicCharacter:
+				if event.get(property).resource_path in renames:
+					event.set(property, load(renames[event.get(property).resource_path]))
+			elif typeof(event.get(property)) == TYPE_STRING and event.get(property) in renames:
+				event.set(property, renames[event.get(property)])
+	ResourceSaver.save(path, timeline)
+	return OK
