4ff71a0aaa69286e4869dd1973963b2842c786aa
Emilio Coppola <coppolaemilio@protonmail.com>
2022-08-07 16:37:52 +0200

    Initial work on static types

--- addons/dialogic/Editor/Common/Toolbar.gd
+++ addons/dialogic/Editor/Common/Toolbar.gd
@@ -1,15 +1,15 @@
 @tool
 extends HBoxContainer
 
-signal toggle_editor_view(mode)
+signal toggle_editor_view(mode:String)
 signal create_timeline
 signal play_timeline
 
 func _ready():
 	# Get version number
 	$Version.set("custom_colors/font_color", get_theme_color("disabled_font_color", "Editor"))
-	var config = ConfigFile.new()
-	var err = config.load("res://addons/dialogic/plugin.cfg")
+	var config := ConfigFile.new()
+	var err := config.load("res://addons/dialogic/plugin.cfg")
 	if err == OK:
 		$Version.text = "v" + config.get_value("plugin", "version")
 	
@@ -31,11 +31,11 @@ func _ready():
 ##							HELPERS
 ################################################################################
 
-func set_resource_saved():
+func set_resource_saved() -> void:
 	if %ResourcePicker.current_value.ends_with(("(*)")):
 		%ResourcePicker.set_value(%ResourcePicker.current_value.trim_suffix("(*)"))
 
-func set_resource_unsaved():
+func set_resource_unsaved() -> void:
 	if not %ResourcePicker.current_value.ends_with(("(*)")):
 		%ResourcePicker.set_value(%ResourcePicker.current_value +"(*)")
 
@@ -48,11 +48,11 @@ func is_current_unsaved() -> bool:
 ##							BASICS
 ################################################################################
 
-func _on_AddTimeline_pressed():
+func _on_AddTimeline_pressed() -> void:
 	emit_signal("create_timeline")
 
 
-func _on_AddCharacter_pressed():
+func _on_AddCharacter_pressed() -> void:
 	find_parent('EditorView').godot_file_dialog(
 		get_parent().get_node("CharacterEditor").new_character,
 		'*.dch; DialogicCharacter',
@@ -63,7 +63,7 @@ func _on_AddCharacter_pressed():
 	)
 
 
-func suggest_resources(filter):
+func suggest_resources() -> Dictionary:
 	var suggestions = {}
 	for i in DialogicUtil.get_project_setting('dialogic/editor/last_resources', []):
 		if i.ends_with('.dtl'):
@@ -73,7 +73,7 @@ func suggest_resources(filter):
 	return suggestions
 
 
-func resource_used(path:String):
+func resource_used(path:String) -> void:
 	var used_resources:Array = DialogicUtil.get_project_setting('dialogic/editor/last_resources', [])
 	if path in used_resources:
 		used_resources.erase(path)
@@ -85,14 +85,14 @@ func resource_used(path:String):
 ##							TIMELINE_MODE
 ################################################################################
 
-func load_timeline(timeline_path):
+func load_timeline(timeline_path:String) -> void:
 	resource_used(timeline_path)
 	%ResourcePicker.set_value(DialogicUtil.pretty_name(timeline_path))
 	%ResourcePicker.resource_icon = get_theme_icon("TripleBar", "EditorIcons")
 	$PlayTimeline.show()
 
 
-func _on_play_timeline():
+func _on_play_timeline() -> void:
 	emit_signal('play_timeline')
 	$PlayTimeline.release_focus()
 
@@ -101,14 +101,14 @@ func _on_play_timeline():
 ##							CHARACTER_MODE
 ################################################################################
 
-func load_character(character_path):
+func load_character(character_path:String) -> void:
 	resource_used(character_path)
 	%ResourcePicker.set_value(DialogicUtil.pretty_name(character_path))
 	%ResourcePicker.resource_icon = load("res://addons/dialogic/Editor/Images/Resources/character.svg")
 	$PlayTimeline.hide()
 
 
-func _on_ResourcePicker_value_changed(property_name, value):
+func _on_ResourcePicker_value_changed(property_name, value) -> void:
 	if value:
 		DialogicUtil.get_dialogic_plugin().editor_interface.inspect_object(load(value))
 
@@ -117,8 +117,8 @@ func _on_ResourcePicker_value_changed(property_name, value):
 ##							EDITING MODE
 ################################################################################
 
-func _on_toggle_visual_editor_clicked():
-	var _mode = 'visual'
+func _on_toggle_visual_editor_clicked() -> void:
+	var _mode := 'visual'
 	if DialogicUtil.get_project_setting('dialogic/editor_mode', 'visual') == 'visual':
 		_mode = 'text'
 	ProjectSettings.set_setting('dialogic/editor_mode', _mode)
@@ -126,7 +126,7 @@ func _on_toggle_visual_editor_clicked():
 	update_toggle_button()
 	
 
-func update_toggle_button():
+func update_toggle_button() -> void:
 	$ToggleVisualEditor.icon = get_theme_icon("ThemeDeselectAll", "EditorIcons")
 	# Have to make this hack for the button to resize properly {
 	$ToggleVisualEditor.size = Vector2(0,0)
