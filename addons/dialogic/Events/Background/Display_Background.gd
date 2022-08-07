f1751263f30b78f5e12dc97d520658dbff9f7977
thebardsrc <thebardsrc@gmail.com>
2022-08-07 08:47:41 -0600

    forgot tween changed and it cant be attached to the same place anymore. new tween instead. prints i will remove next commit, testing a performance issue with picture loader without the fade

--- addons/dialogic/Events/Background/Display_Background.gd
+++ addons/dialogic/Events/Background/Display_Background.gd
@@ -2,8 +2,6 @@ extends TextureRect
 
 class_name DialogicDisplay_Background, 'icon.png'
 
-var tween
-
 func _ready():
 	add_to_group('dialogic_bg_image')
 
@@ -14,31 +12,7 @@ func _init():
 	anchor_right = 1
 	anchor_bottom = 1
 	
+
+	
 	# TODO Might not be full screen when resizing window because I had to remove margin setters for godoto4
 
-func fade_in(time = 1):
-	modulate = Color(1, 1,1,0)
-	tween.interpolate_property(self, "modulate",
-		null, Color(1,1,1,1), time,
-		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	tween.start()
-
-
-func fade_out(time = 1):
-	if tween:
-		tween.connect('tween_all_completed', self, '_on_tween_over')
-		tween.interpolate_property(self, "modulate",
-			Color(1,1,1,1), Color(1,1,1,0), time,
-			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-		tween.start()
-	else:
-		_on_tween_over()
-
-func remove_with_delay(time =1):
-	var timer = Timer.new()
-	timer.timeout.connect("timeout", self, "queue_free")
-	add_child(timer)
-	timer.start(time+0.1)
-
-func _on_tween_over():
-	queue_free()
