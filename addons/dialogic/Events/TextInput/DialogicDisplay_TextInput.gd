967aebcd2f7f3e8d330f6f3991aae1df18fad291
Jowan-Spooner <raban-loeffler@posteo.de>
2022-08-24 12:39:26 +0200

    Add TextInput event
    # Text Input event
    Options:
    - Variable (the variable that the input will be stored to)
    - Text (will by default be displayed above the line edit)
    - Default
    - Placeholder
    - Allow Empty
    
    *Is missing an icon*
    
    # Text input display node
    A control node that has three exported node paths to a text label, an input line edit and a confirmation button.
    
    A node of this type along the needed child nodes was added to the DefaultDialogNode scene.

---
+++ addons/dialogic/Events/TextInput/DialogicDisplay_TextInput.gd
@@ -0,0 +1,45 @@
+extends Control
+
+class_name DialogicDisplay_TextInput
+
+@export_node_path var input_line_edit:NodePath
+@export_node_path var text_label:NodePath
+@export_node_path var confirmation_button:NodePath
+
+var allow_empty : bool = false
+
+func _ready():
+	add_to_group('dialogic_text_input')
+	if confirmation_button:
+		get_node(confirmation_button).pressed.connect(_on_confirmation_button_pressed)
+	if input_line_edit:
+		get_node(input_line_edit).text_changed.connect(_on_input_text_changed)
+	hide()
+
+func set_text(text:String) -> void:
+	if get_node(text_label) is Label:
+		get_node(text_label).text = text
+
+func set_placeholder(placeholder:String) -> void:
+	if get_node(input_line_edit) is LineEdit:
+		get_node(input_line_edit).placeholder_text = placeholder
+		get_node(input_line_edit).grab_focus()
+
+func set_default(default:String) -> void:
+	if get_node(input_line_edit) is LineEdit:
+		get_node(input_line_edit).text = default
+		_on_input_text_changed(default)
+
+
+func set_allow_empty(boolean:bool) -> void:
+	allow_empty = boolean
+
+func _on_input_text_changed(text:String) -> void:
+	if confirmation_button.is_empty():
+		return
+	get_node(confirmation_button).disabled = !allow_empty and text.is_empty()
+
+func _on_confirmation_button_pressed():
+	if get_node(input_line_edit) is LineEdit:
+		if !get_node(input_line_edit).text.is_empty() or allow_empty:
+			Dialogic.TextInput.input_confirmed.emit(get_node(input_line_edit).text)
