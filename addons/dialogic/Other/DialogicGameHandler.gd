a82aafdcc84f96e464c78459b425dc2dfee7e604
thebardsrc <thebardsrc@gmail.com>
2022-08-25 23:45:57 -0600

    actually lets make this one a public function, not private

--- addons/dialogic/Other/DialogicGameHandler.gd
+++ addons/dialogic/Other/DialogicGameHandler.gd
@@ -71,12 +71,13 @@ func start_timeline(timeline_resource:Variant, label_or_idx:Variant = "") -> voi
 		
 	# load the resource if only the path is given
 	if typeof(timeline_resource) == TYPE_STRING:
+		print(str(Time.get_ticks_msec()) + ": Loading timeline")	
 		timeline_resource = load(timeline_resource)
+		print(str(Time.get_ticks_msec()) + ": Finished loading timeline")	
 		if timeline_resource == null:
 			assert(false, "There was an error loading this timeline. Check the filename, and the timeline for errors")
 	
-	if timeline_resource._events_processed == false:
-		timeline_resource = _process_unloaded_timeline(timeline_resource)
+	process_timeline(timeline_resource)
 		
 	current_timeline = timeline_resource
 	current_timeline_events = current_timeline.get_events()
@@ -334,73 +335,77 @@ func _thread_deferred_timeline_items() -> void:
 func _thread_deferred_preload_timeline() -> void:
 	pass
 
-func _process_unloaded_timeline(timeline: DialogicTimeline) -> DialogicTimeline:
-	print(str(Time.get_ticks_msec()) + ": Starting process unloaded timeline")	
-	var prev_indent := ""
-	var events := []
-	
-	# this is needed to add a end branch event even to empty conditions/choices
-	var prev_was_opener := false
-	
-	var lines := timeline._events
-	var idx := -1
-	
-	while idx < len(lines)-1:
-		idx += 1
-		var line :String = lines[idx]
+func process_timeline(timeline: DialogicTimeline) -> DialogicTimeline:
+#	print(str(Time.get_ticks_msec()) + ": Starting process unloaded timeline")	
+	if timeline._events_processed:
+		return timeline
+	else:
+		var prev_indent := ""
+		var events := []
 		
+		# this is needed to add a end branch event even to empty conditions/choices
+		var prev_was_opener := false
 		
-		var line_stripped :String = line.strip_edges(true, false)
-		if line_stripped.is_empty():
-			continue
-		var indent :String= line.substr(0,len(line)-len(line_stripped))
+		var lines := timeline._events
+		var idx := -1
 		
-		if len(indent) < len(prev_indent):
-			for i in range(len(prev_indent)-len(indent)):
+		while idx < len(lines)-1:
+			idx += 1
+			var line :String = lines[idx]
+			
+			
+			var line_stripped :String = line.strip_edges(true, false)
+			if line_stripped.is_empty():
+				continue
+			var indent :String= line.substr(0,len(line)-len(line_stripped))
+			
+			if len(indent) < len(prev_indent):
+				for i in range(len(prev_indent)-len(indent)):
+					events.append(DialogicEndBranchEvent.new())
+			
+			elif prev_was_opener and len(indent) == len(prev_indent):
+				events.append(DialogicEndBranchEvent.new())
+			prev_indent = indent
+			var event_content :String = line_stripped
+
+			var event :DialogicEvent
+			for i in _event_script_cache:
+				if i._test_event_string(event_content):
+					event = i.duplicate()
+					break
+			
+			# add the following lines until the event says it's full there is an empty line or the indent changes
+			while !event.is_string_full_event(event_content):
+				idx += 1
+				if idx == len(lines):
+					break
+				var following_line :String = lines[idx]
+				var following_line_stripped :String = following_line.strip_edges(true, false)
+				var following_line_indent :String = following_line.substr(0,len(following_line)-len(following_line_stripped))
+				if following_line_stripped.is_empty():
+					break
+				if following_line_indent != indent:
+					idx -= 1
+					break
+				event_content += "\n"+following_line_stripped
+			
+			event_content = event_content.replace("\n"+indent, "\n")
+			
+
+			event._load_from_string(event_content)
+
+			events.append(event)
+			prev_was_opener = event.can_contain_events
+			
+		
+
+		if !prev_indent.is_empty():
+			for i in range(len(prev_indent)):
 				events.append(DialogicEndBranchEvent.new())
 		
-		elif prev_was_opener and len(indent) == len(prev_indent):
-			events.append(DialogicEndBranchEvent.new())
-		prev_indent = indent
-		var event_content :String = line_stripped
-
-		var event :DialogicEvent
-		for i in _event_script_cache:
-			if i._test_event_string(event_content):
-				event = i.duplicate()
-				break
-		
-		# add the following lines until the event says it's full there is an empty line or the indent changes
-		while !event.is_string_full_event(event_content):
-			idx += 1
-			if idx == len(lines):
-				break
-			var following_line :String = lines[idx]
-			var following_line_stripped :String = following_line.strip_edges(true, false)
-			var following_line_indent :String = following_line.substr(0,len(following_line)-len(following_line_stripped))
-			if following_line_stripped.is_empty():
-				break
-			if following_line_indent != indent:
-				idx -= 1
-				break
-			event_content += "\n"+following_line_stripped
-		
-		event_content = event_content.replace("\n"+indent, "\n")
-		
-
-		event._load_from_string(event_content)
-
-		events.append(event)
-		prev_was_opener = event.can_contain_events
-		
-	
-
-	if !prev_indent.is_empty():
-		for i in range(len(prev_indent)):
-			events.append(DialogicEndBranchEvent.new())
-	
-	timeline._events = events	
-	print(str(Time.get_ticks_msec()) + ": Finished process unloaded timeline")	
-	return timeline
+		timeline._events = events	
+		timeline._events_processed = true
+		#print(str(Time.get_ticks_msec()) + ": Finished process unloaded timeline")	
+		return timeline
 	
 
