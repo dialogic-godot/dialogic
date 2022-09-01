737828b4dd7984f1912fdb6f63180303d4d22cff
Jowan-Spooner <raban-loeffler@posteo.de>
2022-09-01 13:02:50 +0200

    Fix/Improve background fade behaviour
    Changed the background fade behaviour. 
    
    This also allows to implement a _fade_in(@time) and _fade_out(@time) on custom background scenes. By default it tries to tween the modulation from transparent to white.

--- addons/dialogic/Events/Background/Subsystem_Backgrounds.gd
+++ addons/dialogic/Events/Background/Subsystem_Backgrounds.gd
@@ -18,25 +18,34 @@ func update_background(path:String = '', fade_time:float = 0.0) -> void:
 	dialogic.current_state_info['background'] = path
 	for node in get_tree().get_nodes_in_group('dialogic_bg_image'):
 		if node.is_visible_in_tree():
-			for child in node.get_children():
-				child.queue_free()
-				
-			# Custom scene's will need to support their own fades
-			# We should probably make a signal here to send into them with the data if they want to use it
-			if path.ends_with('.tscn'):
-				node.add_child(load(path).instantiate())
 			
-			elif fade_time > 0: 
-				node.add_child(node.duplicate())
-				node.texture = load(path)
-				for child in node.get_children():
-					var tween = child.create_tween()
-					tween.tween_property(child, "modulate", Color.TRANSPARENT, fade_time)
-					tween.tween_callback(child.queue_free)
+			# remove previous backgrounds
+			for old_bg in node.get_children():
+				if old_bg.has_method('_fade_out'):
+					old_bg._fade_out(fade_time)
+				elif "modulate" in old_bg:
+					var tween = old_bg.create_tween()
+					tween.tween_property(old_bg, "modulate", Color.TRANSPARENT, fade_time)
+					tween.tween_callback(old_bg.queue_free)
+				else:
+					old_bg.queue_free()
+			
+			var new_node
+			if path.ends_with('.tscn'):
+				new_node = load(path).instantiate()
+			elif File.file_exists(path):
+				new_node = node.duplicate()
+				new_node.texture = load(path)
+			else:
+				new_node = null
+			
+			if new_node:
+				node.add_child(new_node)
 				
-			else:	
-				node.texture = null
-				if not path.is_empty():
-					node.texture = load(path)
+				if new_node.has_method('_fade_in'):
+					new_node._fade_in(fade_time)
 					
-
+				elif "modulate" in new_node:
+					new_node.modulate = Color.TRANSPARENT
+					var tween = new_node.create_tween()
+					tween.tween_property(new_node, "modulate", Color.WHITE, fade_time)
