3b1ef3dd15e877031886a28b273d85e3c450394a
Jowan-Spooner <42868150+Jowan-Spooner@users.noreply.github.com>
2022-12-05 17:36:48 +0100

    Text editor improvements (better highlighting, comment/move lines) (#1345)
    Original work over at #1335 by @nathanhoad. All credit goes to him.
    
    Author: @nathanhoad

--- addons/dialogic/Editor/TimelineEditor/TextEditor/timeline_editor_text.gd
+++ addons/dialogic/Editor/TimelineEditor/TextEditor/timeline_editor_text.gd
@@ -1,16 +1,11 @@
 @tool
 extends CodeEdit
 
-#var current_timeline: DialogicTimeline
+## Sub-Editor that allows editing timelines in a text format.
 
-#var editor_reference = null
-
-#@onready var _toolbar = get_parent().get_parent().get_node('Toolbar')
-
+ 
 func _ready():
 	DialogicUtil.get_dialogic_plugin().dialogic_save.connect(save_timeline)
-#	if find_parent('EditorView'): # This prevents the view to turn black if you are editing this scene in Godot
-#		editor_reference = find_parent('EditorView')
 	add_highlighting()
 
 
@@ -18,13 +13,27 @@ func _on_text_editor_text_changed():
 	get_parent().current_resource_state = DialogicEditor.ResourceStates.Unsaved
 
 
+func _gui_input(event):
+	if not event is InputEventKey: return
+	if not event.is_pressed(): return
+	match event.as_text():
+		"Ctrl+K":
+			toggle_comment()
+		"Alt+Up":
+			move_line(-1)
+		"Alt+Down":
+			move_line(1)
+		_:
+			return
+	get_viewport().set_input_as_handled()
+	
+
 func clear_timeline():
 	text = ''
 
 
 func load_timeline(object:DialogicTimeline) -> void:
 	clear_timeline()
-#	current_timeline = object
 	if get_parent().current_resource.events.size() == 0:
 		pass
 	else: 
@@ -32,7 +41,6 @@ func load_timeline(object:DialogicTimeline) -> void:
 			get_parent().current_resource.events_processed = false
 			get_parent().current_resource = get_parent().editors_manager.resource_helper.process_timeline(get_parent().current_resource)
 	
-#	get_parent().get_parent().get_node('Toolbar').load_timeline(get_parent().current_resource.resource_path)
 	
 	#text = TimelineUtil.events_to_text(object.events)
 	var result:String = ""	
@@ -48,8 +56,8 @@ func load_timeline(object:DialogicTimeline) -> void:
 			result += "\t".repeat(indent)+event['event_node_as_text'].replace('\n', "\n"+"\t".repeat(indent)) + "\n"
 		if event.can_contain_events:
 			indent += 1
-		if indent < 0: indent = 0
-		result += "\t".repeat(indent)+"\n"
+		if indent < 0: 
+			indent = 0
 		
 	text = result
 	get_parent().current_resource.set_meta("timeline_not_saved", false)
@@ -80,21 +88,40 @@ func save_timeline():
 func add_highlighting():
 	# This is a dumpster fire, so hopefully it will be improved during beta?
 	var editor_settings = DialogicUtil.get_dialogic_plugin().editor_interface.get_editor_settings()
+	var keywords_color: Color = editor_settings.get('text_editor/theme/highlighting/keyword_color')
+	var functions_color: Color = editor_settings.get('text_editor/theme/highlighting/function_color')
+	var strings_color: Color = editor_settings.get('text_editor/theme/highlighting/string_color')
+	var numbers_color: Color = editor_settings.get("text_editor/theme/highlighting/number_color")
+	var types_color: Color = editor_settings.get('text_editor/theme/highlighting/engine_type_color')
+	var jumps_color: Color = editor_settings.get("text_editor/theme/highlighting/control_flow_keyword_color")
+	var symbols_color: Color = editor_settings.get('text_editor/theme/highlighting/text_color')
+	var text_color: Color = editor_settings.get('text_editor/theme/highlighting/text_color')
+	var comments_color: Color = editor_settings.get('text_editor/theme/highlighting/comment_color')
+	
 	var s := CodeHighlighter.new()
+	
 	s.color_regions = {
-		'[ ]': editor_settings.get('text_editor/theme/highlighting/function_color'),
-		'< >': editor_settings.get('text_editor/theme/highlighting/function_color'),
-		'" "': editor_settings.get('text_editor/theme/highlighting/string_color'),
-		'{ }': editor_settings.get('text_editor/theme/highlighting/engine_type_color'),
+		'[ ]': functions_color,
+		'< >': functions_color,
+		'" "': strings_color,
+		'{ }': types_color
 	}
-	#s.keyword_colors = {
-	#	'jump': Color('#00abc7')
-	#}
-	s.symbol_color = editor_settings.get('text_editor/theme/highlighting/text_color')
-	s.number_color = editor_settings.get('text_editor/theme/highlighting/text_color')
-	s.member_variable_color = editor_settings.get('text_editor/theme/highlighting/text_color')
-	s.function_color = editor_settings.get('text_editor/theme/highlighting/text_color')
-	s.add_color_region('- ', '', editor_settings.get('text_editor/theme/highlighting/engine_type_color'), true)
+	s.add_color_region('- ', '', types_color, true)
+	s.add_color_region('# ', '', comments_color, true)
+	s.add_color_region(': ', '', text_color, true)
+	
+	s.keyword_colors = {
+		"if": keywords_color,
+		"elif": keywords_color,
+		"else": keywords_color,
+		"and": keywords_color,
+		"or": keywords_color
+	}
+	
+	s.symbol_color = symbols_color
+	s.number_color = numbers_color
+	s.member_variable_color = text_color
+	s.function_color = text_color
 	set('syntax_highlighter', s)
 
 
@@ -120,4 +147,57 @@ func text_timeline_to_array(text:String) -> Array:
 	return events
 
 
+# Toggle the selected lines as comments
+func toggle_comment() -> void:
+	var cursor: Vector2 = Vector2(get_caret_column(), get_caret_line())
+	var from: int = cursor.y
+	var to: int = cursor.y
+	if has_selection():
+		from = get_selection_from_line()
+		to = get_selection_to_line()
 
+	var lines: PackedStringArray = text.split("\n")
+	var will_comment: bool = not lines[from].begins_with("# ")
+	for i in range(from, to + 1):
+		lines[i] = "# " + lines[i] if will_comment else lines[i].substr(2)
+
+	text = "\n".join(lines)
+	select(from, 0, to, get_line_width(to))
+	set_caret_line(cursor.y)
+	set_caret_column(cursor.x)
+	text_changed.emit()
+
+
+# Move the selected lines up or down
+func move_line(offset: int) -> void:
+	offset = clamp(offset, -1, 1)
+
+	var cursor: Vector2 = Vector2(get_caret_column(), get_caret_line())
+	var reselect: bool = false
+	var from: int = cursor.y
+	var to: int = cursor.y
+	if has_selection():
+		reselect = true
+		from = get_selection_from_line()
+		to = get_selection_to_line()
+
+	var lines := text.split("\n")
+
+	if from + offset < 0 or to + offset >= lines.size(): return
+
+	var target_from_index: int = from - 1 if offset == -1 else to + 1
+	var target_to_index: int = to if offset == -1 else from
+	var line_to_move: String = lines[target_from_index]
+	lines.remove_at(target_from_index)
+	lines.insert(target_to_index, line_to_move)
+
+	text = "\n".join(lines)
+
+	cursor.y += offset
+	from += offset
+	to += offset
+	if reselect:
+		select(from, 0, to, get_line_width(to))
+	set_caret_line(cursor.y)
+	set_caret_column(cursor.x)
+	text_changed.emit()
