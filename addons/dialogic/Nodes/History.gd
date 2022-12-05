56b321237240ebd0bd1712bfb249d4d649e62cea
eaganlai <74284983+eaganlai@users.noreply.github.com>
2022-12-05 14:02:43 +0000

    Revert "Fixed Speed tag resolve in HistoryRow"
    This reverts commit 793648377a8be2b579545fb29c257adb59f15fd1.

--- addons/dialogic/Nodes/History.gd
+++ addons/dialogic/Nodes/History.gd
@@ -222,11 +222,7 @@ func add_history_row_event(eventData):
 	# event logging handled here
 	# Text Events, replacing br with linebreaks
 	if eventData.event_id == 'dialogic_001':
-		eventData.text = eventData.text.replace('[br]', '\n')
-		var regex = RegEx.new()
-		regex.compile("\\[[^\\]]{0,}speed[^\\]]{0,}\\]")
-		eventData.text = regex.sub(eventData.text, "")
-		newHistoryRow.add_history(str(characterPrefix, eventData.text), audioData)
+		newHistoryRow.add_history(str(characterPrefix, eventData.text.replace('[br]', '\n')), audioData)
 	# Character Arrivals
 	elif eventData.event_id == 'dialogic_002':
 		var logText = get_parent().settings.get_value('history', 'text_arrivals', 'has arrived')
