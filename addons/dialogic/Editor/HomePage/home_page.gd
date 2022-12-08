c0fe4f21e47a6f3c1fd917f0aeded1e0032a6b66
Jowan Spooner <raban-loeffler@posteo.de>
2022-12-08 23:50:22 +0100

    Improve HomePage look

--- addons/dialogic/Editor/HomePage/home_page.gd
+++ addons/dialogic/Editor/HomePage/home_page.gd
@@ -3,6 +3,19 @@ extends DialogicEditor
 
 ## A Main page in the dialogic editor.
 
+func _ready():
+	var edit_scale := DialogicUtil.get_editor_scale()
+	var min_height := 0
+	for box in %BoxHolder.get_children():
+		box.self_modulate = get_theme_color("accent_color", "Editor")
+		box.custom_minimum_size.x = 220 * edit_scale
+		if box.get_index() < ceilf(%BoxHolder.get_child_count()/2.0):
+			min_height += box.size.y
+	
+	%VersionLabel.set('theme_override_font_sizes/font_size', 20 * edit_scale)
+	$CenterContainer/ScrollContainer.custom_minimum_size.x = 440 *edit_scale
+	%BoxHolder.custom_minimum_size.y = min_height+100*edit_scale
+	set('theme_override_constants/separation', 30*edit_scale)
 
 
 func _register():
@@ -25,3 +38,17 @@ func _on_discord_button_pressed() -> void:
 
 func _on_wiki_button_pressed() -> void:
 	OS.shell_open("https://github.com/coppolaemilio/dialogic/wiki")
+
+
+func _on_wiki_box_button_pressed():
+	OS.shell_open("https://github.com/coppolaemilio/dialogic/wiki/Tutorial:-Getting-Started")
+
+
+func _on_sharing_box_button_pressed():
+	_on_discord_button_pressed()
+
+func _on_bug_box_button_pressed():
+	OS.shell_open("https://github.com/coppolaemilio/dialogic/issues/new/choose")
+
+func _on_restart_box_button_pressed():
+	find_parent('EditorView').plugin_reference.get_editor_interface().restart_editor()
