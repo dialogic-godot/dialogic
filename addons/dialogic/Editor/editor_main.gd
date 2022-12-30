5b28d1064feb95e2e6636bafa1810f8d51a4291b
Emilio Coppola <coppolaemilio@protonmail.com>
2022-12-30 14:20:43 +0100

    Small visual update

--- addons/dialogic/Editor/editor_main.gd
+++ addons/dialogic/Editor/editor_main.gd
@@ -17,7 +17,7 @@ func _ready():
 	editors_manager = $Margin/EditorsManager
 	
 	## STYLING
-	color = get_theme_color("dark_color_2", "Editor")
+	color = get_theme_color("base_color", "Editor")
 	editor_tab_bg.border_color = get_theme_color("base_color", "Editor")
 	$Margin/EditorsManager/HSplit/Editors.add_theme_stylebox_override('panel', editor_tab_bg)
 	
