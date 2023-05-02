ac0393a2793f0849f5e3d09733dc317e0ef686b1
Jowan-Spooner <raban-loeffler@posteo.de>
2023-05-02 12:40:15 +0200

    Better new text animation

--- addons/dialogic/Events/DefaultStyles/Default/animations.gd
+++ addons/dialogic/Events/DefaultStyles/Default/animations.gd
@@ -1,28 +1,26 @@
 extends AnimationPlayer
 
+# A custom script/node that adds some animations to the textbox.
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	Dialogic.Text.animation_textbox_hide.connect(_on_textbox_hide)
 	Dialogic.Text.animation_textbox_show.connect(_on_textbox_show)
 	Dialogic.Text.animation_textbox_new_text.connect(_on_textbox_new_text)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_textbox_show():
 	Dialogic.Animation.start_animating()
+	%DialogicNode_DialogText.text = ""
 	get_node("../DialogTextAnimationParent").modulate = Color.TRANSPARENT
 	play("text_box_reveal")
 	animation_finished.connect(Dialogic.Animation.animation_finished, CONNECT_ONE_SHOT)
+
 
 func _on_textbox_hide():
 	Dialogic.Animation.start_animating()
 	play_backwards("text_box_reveal")
 	animation_finished.connect(Dialogic.Animation.animation_finished, CONNECT_ONE_SHOT)
+
 
 func _on_textbox_new_text():
 	Dialogic.Animation.start_animating()
