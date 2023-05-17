c37e29ccbc908291709532e47c78d437b18d8c3b
Jowan-Spooner <42868150+Jowan-Spooner@users.noreply.github.com>
2023-05-17 20:50:34 +0200

    Fix example portrait animated sprite (#1559)
    Some methods and properties changed since this was last looked at as pointed out in #1557

--- addons/dialogic/Example Assets/portraits/CustomPortrait_AnimatedSprite.gd
+++ addons/dialogic/Example Assets/portraits/CustomPortrait_AnimatedSprite.gd
@@ -6,12 +6,13 @@ func _update_portrait(passed_character:DialogicCharacter, passed_portrait:String
 	if passed_portrait == "":
 		passed_portrait = passed_character['default_portrait']
 	
-	if $Sprite.frames.has_animation(passed_portrait):
+	if $Sprite.sprite_frames.has_animation(passed_portrait):
 		$Sprite.play(passed_portrait)
 
 func _on_animated_sprite_2d_animation_finished():
-	$Sprite.frame = randi()%$Sprite.frames.get_frame_count($Sprite.animation)
+	$Sprite.frame = randi()%$Sprite.sprite_frames.get_frame_count($Sprite.animation)
 	$Sprite.play()
 
+
 func _get_covered_rect() -> Rect2:
-	return Rect2($Sprite.position, $Sprite.frames.get_frame($Sprite.animation, 0).get_size()*$Sprite.scale)
+	return Rect2($Sprite.position, $Sprite.sprite_frames.get_frame_texture($Sprite.animation, 0).get_size()*$Sprite.scale)
