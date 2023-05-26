b9b0573d009b8b3dec1336473112bc09321bd0ef
Jowan-Spooner <raban-loeffler@posteo.de>
2023-05-26 23:25:35 +0200

    Add Settings to text bubble layout

--- addons/dialogic/Modules/DefaultStyles/TextBubble/TextBubble.gd
+++ addons/dialogic/Modules/DefaultStyles/TextBubble/TextBubble.gd
@@ -3,15 +3,15 @@ extends Control
 @onready var tail : Line2D = $Tail
 @onready var bubble : Control = $Background
 var speaker_node : Node = null
-
+var character : DialogicCharacter = null
 var max_width := 300
 
 var bubble_rect : Rect2 = Rect2(0.0, 0.0, 2.0, 2.0)
-var base_position = Vector2.ZERO
+var base_position := Vector2.ZERO
 
-var base_direction = Vector2(1.0, -1.0).normalized()
-var safe_zone = 50.0
-
+var base_direction := Vector2(1.0, -1.0).normalized()
+var safe_zone := 50.0
+var padding := Vector2()
 
 func _ready() -> void:
 	scale = Vector2.ZERO
@@ -24,19 +24,19 @@ func _process(delta):
 	if speaker_node: 
 		base_position = speaker_node.get_global_transform_with_canvas().origin
 	
-	var center = get_viewport_rect().size / 2.0
+	var center := get_viewport_rect().size / 2.0
 	
-	var dist_x = abs(base_position.x - center.x)
-	var dist_y = abs(base_position.y - center.y)
-	var x_e = center.x - bubble_rect.size.x
-	var y_e = center.y - bubble_rect.size.y
-	var influence_x = remap(clamp(dist_x, x_e, center.x), x_e, center.x * 0.8, 0.0, 1.0)
-	var influence_y = remap(clamp(dist_y, y_e, center.y), y_e, center.y * 0.8, 0.0, 1.0)
+	var dist_x := abs(base_position.x - center.x)
+	var dist_y := abs(base_position.y - center.y)
+	var x_e := center.x - bubble_rect.size.x
+	var y_e := center.y - bubble_rect.size.y
+	var influence_x := remap(clamp(dist_x, x_e, center.x), x_e, center.x * 0.8, 0.0, 1.0)
+	var influence_y := remap(clamp(dist_y, y_e, center.y), y_e, center.y * 0.8, 0.0, 1.0)
 	if base_position.x > center.x: influence_x = -influence_x
 	if base_position.y > center.y: influence_y = -influence_y
-	var edge_influence = Vector2(influence_x, influence_y)
+	var edge_influence := Vector2(influence_x, influence_y)
 	
-	var direction = (base_direction + edge_influence).normalized()
+	var direction := (base_direction + edge_influence).normalized()
 	
 	var p : Vector2 = base_position + direction * (safe_zone + lerp(bubble_rect.size.y, bubble_rect.size.x, abs(direction.x)) * 0.4)
 	p = p.clamp(bubble_rect.size / 2.0, get_viewport_rect().size - bubble_rect.size / 2.0)
@@ -51,8 +51,8 @@ func _process(delta):
 	point_a += offset
 	point_b += offset * 0.5
 	
-	var curve = Curve2D.new()
-	var direction_point = Vector2(0, (point_b.y - point_a.y))
+	var curve := Curve2D.new()
+	var direction_point := Vector2(0, (point_b.y - point_a.y))
 	curve.add_point(point_a, Vector2.ZERO, direction_point * 0.5)
 	curve.add_point(point_b)
 	tail.points = curve.tessellate(5)
@@ -78,29 +78,27 @@ func close() -> void:
 
 
 func _on_dialog_text_started_revealing_text():
-	var font = %DialogText.get_theme_font("normal_font")
-	var line_height = font.get_height()
-	%DialogText.size = font.get_string_size(%DialogText.get_parsed_text())
-	if %DialogText.size.x > max_width:
-		%DialogText.size.y = line_height*ceil(%DialogText.size.x/max_width)
-		%DialogText.size.x = max_width
+	var font :Font = %DialogText.get_theme_font("normal_font")
+	%DialogText.size = font.get_multiline_string_size(%DialogText.get_parsed_text(), HORIZONTAL_ALIGNMENT_LEFT, max_width, %DialogText.get_theme_font_size("normal_font_size"))
 	if Dialogic.Choices.is_question(Dialogic.current_event_idx):
-		%DialogText.size.y += 30
+		font = $DialogText/ChoiceContainer/DialogicNode_ChoiceButton.get_theme_font('font')
+		%DialogText.size.y += font.get_string_size(%DialogText.get_parsed_text(), HORIZONTAL_ALIGNMENT_LEFT, max_width, $DialogText/ChoiceContainer/DialogicNode_ChoiceButton.get_theme_font_size("font_size")).y
 	%DialogText.position = -%DialogText.size/2
 	
 	_resize_bubble()
 
 
 func _resize_bubble() -> void:
-	var bubble_size = %DialogText.size+Vector2(20,20)
-	var half_size = (bubble_size / 2.0)
+	var bubble_size :Vector2 = %DialogText.size+(padding*2)
+	var half_size :Vector2= (bubble_size / 2.0)
 	%DialogText.pivot_offset = half_size
 	bubble.pivot_offset = half_size
-	
 	bubble_rect = Rect2(position, bubble_size * Vector2(1.1, 1.1))
+	bubble.size = bubble_size
+	bubble.position = -half_size
 	
 	var t := create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
-	t.tween_property(bubble, "custom_minimum_size", bubble_rect.size, 0.2)
+	t.tween_property(bubble, "scale", Vector2.ONE, 0.2).from(Vector2.ZERO)
 	
 	# set bubble's ratio
 	var bubble_ratio := Vector2.ONE
