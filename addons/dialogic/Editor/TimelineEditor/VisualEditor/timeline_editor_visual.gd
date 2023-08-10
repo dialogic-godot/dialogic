dd329adba86bd9caef8a04e5f735bbe9ac653bad
MetaNoder <117393840+metanoder@users.noreply.github.com>
2023-08-10 17:12:02 +0000

    Hotkeys - option key 4 mac added

--- addons/dialogic/Editor/TimelineEditor/VisualEditor/timeline_editor_visual.gd
+++ addons/dialogic/Editor/TimelineEditor/VisualEditor/timeline_editor_visual.gd
@@ -346,21 +346,23 @@ func _input(event:InputEvent) -> void:
 			_add_event_button_pressed(DialogicTextEvent.new(), true)
 			get_viewport().set_input_as_handled()
 		
-		"Ctrl+Shift+T", "Ctrl+Alt+T": # Add text event with current or previous character 
+		"Ctrl+Shift+T", "Ctrl+Alt+T", "Ctrl+Option+T": # Add text event with current or previous character 
 			get_viewport().set_input_as_handled()
 			var ev := DialogicTextEvent.new()
-			ev.character = get_previous_character(event.as_text() == "Ctrl+Alt+T")
+			ev.character = get_previous_character(event.as_text() == "Ctrl+Alt+T" || "Ctrl+Option+T")
 			_add_event_button_pressed(ev, true)
 		
 		"Ctrl+E": # Add character join event
 			_add_event_button_pressed(DialogicCharacterEvent.new(), true)
 			get_viewport().set_input_as_handled()
+
 		"Ctrl+Shift+E": # Add character update event
 			var ev := DialogicCharacterEvent.new()
 			ev.action = DialogicCharacterEvent.Actions.UPDATE
 			_add_event_button_pressed(ev, true)
 			get_viewport().set_input_as_handled()
-		"Ctrl+Alt+E": # Add character leave event
+
+		"Ctrl+Alt+E", "Ctrl+Option+E": # Add character leave event
 			var ev := DialogicCharacterEvent.new()
 			ev.action = DialogicCharacterEvent.Actions.LEAVE
 			_add_event_button_pressed(ev, true)
