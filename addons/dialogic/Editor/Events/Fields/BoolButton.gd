5a1ece64b356118131ac1ff4323b59bfdddd56a2
Jowan-Spooner <raban-loeffler@posteo.de>
2023-08-23 00:13:05 +0200

    Fix character event update mode behaviour
    The functionality works great now, it's just UI that's pretty messy now. Hope I can get some help with that.

---
+++ addons/dialogic/Editor/Events/Fields/BoolButton.gd
@@ -0,0 +1,19 @@
+@tool
+extends Button
+
+## Event block field for boolean values.
+
+signal value_changed
+var property_name : String
+
+
+func _ready() -> void:
+	toggled.connect(_on_value_changed)
+
+
+func set_value(value:bool) -> void:
+	button_pressed = value
+
+
+func _on_value_changed(value:bool) -> void:
+	value_changed.emit(property_name, value)
