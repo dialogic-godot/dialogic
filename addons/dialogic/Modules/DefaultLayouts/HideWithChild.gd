46248b2f7d278aaaf94e81aeff1b1c122fa90268
Jowan-Spooner <raban-loeffler@posteo.de>
2023-08-30 12:03:20 +0200

    Add name_label_root setting
    This makes the hideWithScript script obsolete. Works the same as on the dialog text node now.

--- addons/dialogic/Modules/DefaultLayouts/HideWithChild.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func _ready():
-	get_child(0).visibility_changed.connect(_on_child_visibility_changed)
-	_on_child_visibility_changed()
-
-func _on_child_visibility_changed():
-	visible = get_child(0).visible
--- addons/dialogic/Modules/Text/node_name_label.gd
+++ addons/dialogic/Modules/Text/node_name_label.gd
@@ -4,20 +4,20 @@ class_name DialogicNode_NameLabel
 
 # If true, the label will be hidden if no character speaks.
 @export var hide_when_empty := true
-
+@export var name_label_root :Node = self
 @export var use_character_color := true
 
 func _ready():
 	add_to_group('dialogic_name_label')
 	if hide_when_empty:
-		visible = false
+		name_label_root.visible = false
 	text = ""
 	
 func _set(property, what):
 	if property == 'text' and typeof(what) == TYPE_STRING:
 		text = what
 		if hide_when_empty:
-			visible = !what.is_empty()
+			name_label_root.visible = !what.is_empty()
 		else:
-			show()
+			name_label_root.show()
 		return true
