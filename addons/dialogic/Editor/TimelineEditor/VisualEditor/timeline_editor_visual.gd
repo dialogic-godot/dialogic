6d00dde7761cd2c1af1d7880c09cea8cec3fb4a7
Jowan-Spooner <42868150+Jowan-Spooner@users.noreply.github.com>
2023-10-07 16:18:26 +0200

    Very small fixes for choices and block movement (#1787)
    * Very small fixes for choices and block movement
    
    * Remove unused method

--- addons/dialogic/Editor/TimelineEditor/VisualEditor/timeline_editor_visual.gd
+++ addons/dialogic/Editor/TimelineEditor/VisualEditor/timeline_editor_visual.gd
@@ -311,7 +311,8 @@ func _on_timeline_area_drag_completed(type:int, index:int, data:Variant) -> void
 		TimelineUndoRedo.commit_action()
 	
 	elif type == %TimelineArea.DragTypes.EXISTING_EVENTS:
-		move_blocks_to_index(data, index)
+		if not (len(data) == 1 and data[0].get_index()+1 == index):
+			move_blocks_to_index(data, index)
 	
 	await get_tree().process_frame
 	something_changed()
