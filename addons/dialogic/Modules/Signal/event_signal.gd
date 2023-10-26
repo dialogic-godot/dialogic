bc529caa79a27dabd0fd97cb921b11992b79c2a0
Pheubel <luca.guagliardo@live.nl>
2023-10-27 01:45:41 +0200

    Allow signal to optionally send a dictionary as argument

--- addons/dialogic/Modules/Signal/event_signal.gd
+++ addons/dialogic/Modules/Signal/event_signal.gd
@@ -8,16 +8,26 @@ extends DialogicEvent
 
 ### Settings
 
-## The argument that will be provided with the signal.
-var argument: String = ""
+var dictionaryMode : bool = false
 
+## The argument that will be provided with the signal.
+var argument: Variant = ""
 
 ################################################################################
 ## 						EXECUTE
 ################################################################################
 
-func _execute() -> void:
-	dialogic.emit_signal('signal_event', argument)
+func _execute() -> void:	
+	if dictionaryMode:
+		var result = JSON.parse_string(argument) 
+		if result != null:
+			var dict := result as Dictionary
+			dict.make_read_only()
+			dialogic.emit_signal('signal_event', dict)
+		else:
+			push_error("encountered malformed dictionary in signal")
+	else:
+		dialogic.emit_signal('signal_event', argument)
 	finish()
 
 
@@ -43,7 +53,8 @@ func get_shortcode() -> String:
 func get_shortcode_parameters() -> Dictionary:
 	return {
 		#param_name : property_info
-		"arg"		: {"property": "argument", "default": ""},
+		"dictMode"	: {"property": "dictionaryMode", "default": false},
+		"arg"		: {"property": "argument", "default": ""}
 	}
 
 ################################################################################
@@ -51,4 +62,7 @@ func get_shortcode_parameters() -> Dictionary:
 ################################################################################
 
 func build_event_editor():
-	add_header_edit('argument', ValueType.SINGLELINE_TEXT, {'left_text':'Emit with argument', 'autofocus':true})
+	add_header_label("Emit with argument type:")
+	add_header_edit('dictionaryMode',ValueType.BOOL, {'left_text':'string', 'right_text': 'dictionary', 'autofocus':true})
+	add_body_edit('argument', ValueType.KEY_VALUE_PAIRS, {'left_text': 'Emit with dictionary'},'dictionaryMode')
+	add_body_edit('argument', ValueType.SINGLELINE_TEXT, {'left_text':'Emit with argument'}, '!dictionaryMode')
