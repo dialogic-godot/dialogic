758b07f23d772ddb0591530bafda4161d700a968
Pheubel <luca.guagliardo@live.nl>
2023-10-28 16:28:54 +0200

    now correctly handles switching between no background and a background

--- addons/dialogic/Modules/Background/subsystem_backgrounds.gd
+++ addons/dialogic/Modules/Background/subsystem_backgrounds.gd
@@ -4,6 +4,8 @@ extends DialogicSubsystem
 
 signal background_changed(info:Dictionary)
 
+var _tween
+var _tween_callbacks : Array[Callable]
 
 var default_background_scene :PackedScene = load(get_script().resource_path.get_base_dir().path_join('default_background.tscn'))
 ####################################################################################################
@@ -35,49 +37,52 @@ func update_background(scene:String = '', argument:String = '', fade_time:float 
 	var info := {'scene':scene, 'argument':argument, 'fade_time':fade_time, 'same_scene':false}
 	for node in get_tree().get_nodes_in_group('dialogic_background_holders'):
 		if node.visible:
+			var backgrounds_node = node.get_node("Backgrounds")
+			var transition_overlay = node.get_node("Overlay")
+			var mat = transition_overlay.material as ShaderMaterial
+			
+			
+			
 			var bg_set: bool = false
 			if scene == dialogic.current_state_info['background_scene']:
-				for old_bg in node.get_children():
+				for old_bg in backgrounds_node.get_children():
 					if old_bg is DialogicBackground and old_bg._should_do_background_update(argument):
 						old_bg._update_background(argument, fade_time)
 						bg_set = true
 						info['same_scene'] = true
 			if !bg_set:
-				# maybe should look into making this a unique instance if multiple background holders are expected
-				var mat = preload("res://addons/dialogic/Modules/Background/default_background_transition.tres") as ShaderMaterial
-				var transition_overlay := ColorRect.new()
-				var tran_tween := get_tree().create_tween()
 				
 				# make sure material is clean and ready to go
 				mat.set_shader_parameter("progress", 0)
-				mat.set_shader_parameter("previousBackground", null)
+				# swap the next background into previous, as that is now the older frame
+				mat.set_shader_parameter("previousBackground", mat.get_shader_parameter("nextBackground"))
+				
 				mat.set_shader_parameter("nextBackground", null)
+				
+				if _tween:
+					_tween.kill()
+					_execute_callbacks_and_reset()
+				_tween = get_tree().create_tween()
+				
 				
 				# could be implemented as passed by the event
 				#mat.set_shader_parameter("whipeTexture", whipe_texture)	# the direction the whipe takes from black to white
 				#mat.set_shader_parameter("feather", feather)				# the trailing smear left behind when the whipe happens
 				
-				tran_tween.tween_method(func (progress: float):
+				_tween.tween_method(func (progress: float):
 					mat.set_shader_parameter("progress", progress)
 				, 0.0, 1.0, fade_time)
 				
-				# set up transition overlay
-				transition_overlay.anchor_right = 1
-				transition_overlay.anchor_bottom = 1
-				# TODO: maybe make this more modular
-				transition_overlay.material = mat
-				
-				tran_tween.tween_callback(_free_bg_node.bind(transition_overlay))
-				
 				
 				# remove previous backgrounds
-				for old_bg in node.get_children():
+				for old_bg in backgrounds_node.get_children():
 					if old_bg is DialogicBackground: 
 						old_bg._fade_out(fade_time) # left in as it can be used to tell the bg that it faded out
-						mat.set_shader_parameter("previousBackground", old_bg._get_background_texture())
 						
-					# remove the old background after the fade is over
-					tran_tween.tween_callback(_free_bg_node.bind(old_bg))
+						# remove the old background after the fade is over
+						_tween_callbacks.append(_free_node.bind(old_bg))
+					else:
+						_free_node(old_bg)
 					
 					#if !old_bg._fade_out(fade_time) and "modulate" in old_bg:
 					#	var tween := old_bg.create_tween()
@@ -99,14 +104,15 @@ func update_background(scene:String = '', argument:String = '', fade_time:float 
 					new_node = null
 				
 				if new_node:
-					node.add_child(new_node)
+					if "self_modulate" in new_node:
+						new_node.self_modulate = Color.TRANSPARENT
+					
+					backgrounds_node.add_child(new_node)
 					
 					new_node._update_background(argument, fade_time)
-					new_node._fade_in(fade_time) # left in as it can be used to tell the bg that it faded in
+					new_node._fade_in(fade_time) # left in as it can be used to tell the bg that it is fading in
 					
 					mat.set_shader_parameter("nextBackground", new_node._get_background_texture())
-					
-					
 					
 					#if new_node.has_method('_update_background'):
 					#	new_node._update_background(argument, fade_time)
@@ -116,15 +122,27 @@ func update_background(scene:String = '', argument:String = '', fade_time:float 
 					#	var tween := new_node.create_tween()
 					#	tween.tween_property(new_node, "modulate", Color.WHITE, fade_time)
 				
-				node.add_child(transition_overlay)
+				_tween.tween_callback(_execute_callbacks_and_reset)
 	
 	dialogic.current_state_info['background_scene'] = scene
 	dialogic.current_state_info['background_argument'] = argument
 	background_changed.emit(info)
 
-func _free_bg_node(node) -> void:
+
+# safely frees a node without causing a panic from a dangling node
+func _free_node(node) -> void:
 	if node and is_instance_valid(node) and !node.is_queued_for_deletion():
 		node.queue_free()
+
+# executes the callbacks that have to happen after the tween and sets the tween to null
+func _execute_callbacks_and_reset() -> void:
+	for callback in _tween_callbacks:
+		callback.call()
+	
+	_tween_callbacks.clear()
+	
+	if _tween:
+		_tween = null
 
 func has_background() -> bool:
 	return !dialogic.current_state_info['background_scene'].is_empty() or !dialogic.current_state_info['background_argument'].is_empty()
