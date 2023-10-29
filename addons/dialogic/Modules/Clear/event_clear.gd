250a5755707240ccbc9ff1a88f4095cc1584adf8
Cake <cake@cakey.moe>
2023-10-29 07:43:32 +0100

    Implement Auto-Skip for Clear Event.

--- addons/dialogic/Modules/Clear/event_clear.gd
+++ addons/dialogic/Modules/Clear/event_clear.gd
@@ -19,27 +19,33 @@ var clear_background := true
 ################################################################################
 
 func _execute() -> void:
+	var final_time := time
+
+	if Dialogic.Text.auto_skip.enabled:
+		var time_per_event: float = Dialogic.Text.auto_skip.time_per_event
+		final_time = min(time, time_per_event)
+
 	if clear_portraits and dialogic.has_subsystem('Portraits') and len(dialogic.Portraits.get_joined_characters()) != 0:
-		if time == 0:
-			dialogic.Portraits.leave_all_characters(DialogicUtil.guess_animation_file('Instant In Or Out'), time, step_by_step)
+		if final_time == 0:
+			dialogic.Portraits.leave_all_characters(DialogicUtil.guess_animation_file('Instant In Or Out'), final_time, step_by_step)
 		else:
-			dialogic.Portraits.leave_all_characters("", time, step_by_step)
-		if step_by_step: await dialogic.get_tree().create_timer(time).timeout
-		
+			dialogic.Portraits.leave_all_characters("", final_time, step_by_step)
+		if step_by_step: await dialogic.get_tree().create_timer(final_time).timeout
+
 	if clear_background and dialogic.has_subsystem('Backgrounds') and dialogic.Backgrounds.has_background():
-		dialogic.Backgrounds.update_background('', '', time)
-		if step_by_step: await dialogic.get_tree().create_timer(time).timeout
-	
+		dialogic.Backgrounds.update_background('', '', final_time)
+		if step_by_step: await dialogic.get_tree().create_timer(final_time).timeout
+
 	if clear_music and dialogic.has_subsystem('Audio') and dialogic.Audio.has_music():
-		dialogic.Audio.update_music('', 0.0, "", time)
-		if step_by_step: await dialogic.get_tree().create_timer(time).timeout
-	
+		dialogic.Audio.update_music('', 0.0, "", final_time)
+		if step_by_step: await dialogic.get_tree().create_timer(final_time).timeout
+
 	if clear_style and dialogic.has_subsystem('Styles'):
 		dialogic.Styles.add_layout_style()
-	
+
 	if clear_portrait_positions and dialogic.has_subsystem('Portraits'):
 		dialogic.Portraits.reset_all_portrait_positions()
-	
+
 	finish()
 
 
@@ -68,7 +74,7 @@ func get_shortcode_parameters() -> Dictionary:
 		"time"		: {"property": "time",	 			"default": ""},
 		"step"		: {"property": "step_by_step", 		"default": true},
 		"portraits"	: {"property": "clear_portraits", 	"default": true},
-		"music"		: {"property": "clear_music", 		"default": true}, 
+		"music"		: {"property": "clear_music", 		"default": true},
 		"background": {"property": "clear_background", 	"default": true},
 		"positions"	: {"property": "clear_portrait_positions", 	"default": true},
 		"style"		: {"property": "clear_style", 		"default": true},
@@ -81,12 +87,12 @@ func get_shortcode_parameters() -> Dictionary:
 
 func build_event_editor():
 	add_header_label('Clear')
-	
+
 	add_body_edit('time', ValueType.FLOAT, {'left_text':'Time:'})
-	
+
 	add_body_edit('step_by_step', ValueType.BOOL, {'left_text':'Step by Step:'}, 'time > 0')
 	add_body_line_break()
-	
+
 #	add_body_edit('set_z_index', ValueType.BOOL, {'icon':load("res://addons/dialogic/Modules/Character/update_z_index.svg"), 'tooltip':'Change Z-Index'}, "action == Actions.UPDATE")
 	add_body_edit('clear_portraits', ValueType.BOOL, {'left_text':'Clear:', 'icon':load("res://addons/dialogic/Modules/Clear/clear_characters.svg"), 'tooltip':'Clear Portraits'})
 	add_body_edit('clear_background', ValueType.BOOL, {'icon':load("res://addons/dialogic/Modules/Clear/clear_background.svg"), 'tooltip':'Clear Background'})
