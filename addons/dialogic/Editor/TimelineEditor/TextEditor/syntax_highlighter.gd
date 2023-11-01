22cc5b91880adcf3a862c9e7af8bff0a93d9f165
Jowan-Spooner <raban-loeffler@posteo.de>
2023-11-01 14:57:37 +0100

    Implement Do event

--- addons/dialogic/Editor/TimelineEditor/TextEditor/syntax_highlighter.gd
+++ addons/dialogic/Editor/TimelineEditor/TextEditor/syntax_highlighter.gd
@@ -15,10 +15,10 @@ var shortcode_regex := RegEx.create_from_string("\\W*\\[(?<id>\\w*)(?<args>[^\\]
 var shortcode_param_regex := RegEx.create_from_string('((?<parameter>[^\\s=]*)\\s*=\\s*"(?<value>([^=]|\\\\=)*)(?<!\\\\)")')
 
 ## Colors
-var normal_color : Color 
+var normal_color : Color
 var translation_id_color: Color
 
-var code_flow_color : Color 
+var code_flow_color : Color
 var boolean_operator_color : Color
 var variable_color : Color
 var string_color : Color
@@ -36,7 +36,7 @@ func _init():
 		var editor_settings = DialogicUtil.get_dialogic_plugin().get_editor_interface().get_editor_settings()
 		normal_color = editor_settings.get('text_editor/theme/highlighting/text_color')
 		translation_id_color = editor_settings.get('text_editor/theme/highlighting/comment_color')
-		
+
 		code_flow_color = editor_settings.get("text_editor/theme/highlighting/control_flow_keyword_color")
 		boolean_operator_color = code_flow_color.lightened(0.5)
 		variable_color = editor_settings.get('text_editor/theme/highlighting/engine_type_color')
@@ -47,7 +47,7 @@ func _init():
 
 func _get_line_syntax_highlighting(line:int) -> Dictionary:
 	var str_line := get_text_edit().get_line(line)
-	
+
 	if shortcode_events.is_empty():
 		for event in Engine.get_main_loop().get_meta('dialogic_event_cache', []):
 			if event.get_shortcode() != 'default_shortcode':
@@ -57,12 +57,12 @@ func _get_line_syntax_highlighting(line:int) -> Dictionary:
 			if event is DialogicTextEvent:
 				text_event = event
 				text_event.load_text_effects()
-	
+
 	var dict := {}
 	dict[0] = {'color':normal_color}
 
 	dict = color_translation_id(dict, str_line)
-	
+
 	if mode == Modes.FULL_HIGHLIGHTING:
 		if str_line.strip_edges().begins_with("[") and !text_event.text_effects_regex.search(str_line.get_slice(' ', 0)):
 			var result:= shortcode_regex.search(str_line)
@@ -70,17 +70,17 @@ func _get_line_syntax_highlighting(line:int) -> Dictionary:
 				if result.get_string('id') in shortcode_events:
 					dict[result.get_start('id')] = {"color":shortcode_events[result.get_string('id')].event_color.lerp(normal_color, 0.4)}
 					dict[result.get_end('id')] = {"color":normal_color}
-				
+
 					if result.get_string('args'):
 						color_shortcode_content(dict, str_line, result.get_start('args'), result.get_end('args'), shortcode_events[result.get_string('id')].event_color)
 			return fix_dict(dict)
-		
+
 		else:
 			for event in custom_syntax_events:
 				if event.is_valid_event(str_line.strip_edges()):
 					dict = event._get_syntax_highlighting(self, dict, str_line)
 					return fix_dict(dict)
-	
+
 	else:
 		dict = text_event._get_syntax_highlighting(self, dict, str_line)
 	return fix_dict(dict)
@@ -110,8 +110,8 @@ func color_condition(dict:Dictionary, line:String, from:int = 0, to:int = 0) -> 
 		dict = color_word(dict, code_flow_color, line, '<=',  from, to)
 	dict = color_region(dict, variable_color, line, '{', '}', from, to)
 	dict = color_region(dict, string_color, line, '"', '"', from, to)
-	
-	
+
+
 	return dict
 
 
@@ -122,7 +122,7 @@ func color_translation_id(dict:Dictionary, line:String) -> Dictionary:
 
 func color_word(dict:Dictionary, color:Color, line:String, word:String, from:int= 0, to:int = 0) -> Dictionary:
 	word_regex.compile("\\W(?<word>"+word+")\\W")
-	if to <= from: 
+	if to <= from:
 		to = len(line)-1
 	for i in word_regex.search_all(line.substr(from, to-from+2)):
 		dict[i.get_start('word')+from] = {'color':color}
@@ -131,11 +131,16 @@ func color_word(dict:Dictionary, color:Color, line:String, word:String, from:int
 
 
 func color_region(dict:Dictionary, color:Color, line:String, start:String, end:String, from:int = 0, to:int = 0, base_color:Color=normal_color) -> Dictionary:
+	if start in "()[].":
+		start = "\\"+start
+	if end in "()[].":
+		end = "\\"+end
+
 	if end.is_empty():
 		region_regex.compile("(?<!\\\\)"+start+".*")
 	else:
 		region_regex.compile("(?<!\\\\)"+start+"(.(?!"+end+"))*."+end)
-	if to <= from: 
+	if to <= from:
 		to = len(line)-1
 	for region in region_regex.search_all(line.substr(from, to-from+2)):
 		dict[region.get_start()+from] = {'color':color}
@@ -144,7 +149,7 @@ func color_region(dict:Dictionary, color:Color, line:String, start:String, end:S
 
 
 func color_shortcode_content(dict:Dictionary, line:String, from:int = 0, to:int = 0, base_color:=normal_color) -> Dictionary:
-	if to <= from: 
+	if to <= from:
 		to = len(line)-1
 	var args_result:= shortcode_param_regex.search_all(line.substr(from, to-from+2))
 	for x in args_result:
--- addons/dialogic/Modules/CallNode/event_call_node.gd
+++ addons/dialogic/Modules/CallNode/event_call_node.gd
@@ -1,71 +1,52 @@
 @tool
-class_name DialogicCallNodeEvent
+class_name DialogicCallEvent
 extends DialogicEvent
 
 ## Event that allows calling a method in a node or autoload.
 
 ### Settings
 
-## The path to the node you want to call. You can access autoloads directly. No need to add /root.
-var path: String = ""
-## The name of the method to call on the given node.
-var method: String = ""
+## The name of the autoload to call the method on.
+var autoload_name: String = ""
+## The name of the method to call on the given autoload.
+var method: String = "":
+	set(value):
+		method = value
+		update_argument_info()
 ## A list of arguments to give to the call.
-var arguments: Array = []
-## If wait is true, the dialog will only continue once the called method is finished.
-var wait: bool = false
-## If this is true, the method will not be called. 
-## Instead you can activate it during a following text event with the [signal] command.
-## Use together with [signal_name] property.
-var inline: bool = false
-## Only necessary if [inline] is true. Sets the argument to listen for.
-var inline_signal_argument: String
-## Only usefull if [inline] is true. If true, the command can only be used once. 
-## If false it will not stop listening to the signal (with that argument).
-var inline_single_use: bool = true
+var arguments: Array = []:
+	set(value):
+		arguments = value
+		check_arguments_and_update_warning()
+		print(arguments)
 
+var _current_method_arg_hints := {'a':null, 'm':null, 'info':{}}
 
 ################################################################################
 ## 						EXECUTION
 ################################################################################
 
 func _execute() -> void:
-	if inline:
-		dialogic.timeline_ended.connect(_disconnect_signal)
-	
-	if path.begins_with('root'):
-		path = "/"+path
-	if not "/" in path and dialogic.get_node('/root').has_node(path):
-		path = "/root/"+path
-	
-	var n :Node = dialogic.get_node_or_null(path)
-	if n:
-		if n.has_method(method):
-			if inline:
-				dialogic.text_signal.connect(_call_on_signal, CONNECT_PERSIST)
-			elif wait:
-				dialogic.current_state = dialogic.States.WAITING
-				await n.callv(method, arguments)
-				dialogic.current_state = dialogic.States.IDLE
-			else:
-				n.callv(method, arguments)
-	else:
-		printerr('[Dialogic] Call node event failed because of invalid path.')
-	finish()
-
-
-func _call_on_signal(arg:String) -> void:
-	if arg != inline_signal_argument:
+	var autoload = dialogic.get_node('/root/'+autoload_name)
+	if autoload == null:
+		printerr("[Dialogic] Call event failed: Unable to find autoload '",autoload_name,"'")
+		finish()
 		return
-	if inline_single_use:
-		dialogic.disconnect("text_signal", _call_on_signal)
-	var n = dialogic.get_node_or_null(path)
-	n.callv(method, arguments)
 
+	if autoload.has_method(method):
+		var args := []
+		for arg in arguments:
+			if arg is String and arg.begins_with('@'):
+				args.append(dialogic.Expression.execute_string(arg.trim_prefix('@')))
+			else:
+				args.append(arg)
+		dialogic.current_state = dialogic.States.WAITING
+		await autoload.callv(method, args)
+		dialogic.current_state = dialogic.States.IDLE
+	else:
+		printerr("[Dialogic] Call event failed: Autoload doesn't have the method '", method,"'.")
 
-func _disconnect_signal():
-	if dialogic.text_signal.is_connected(_call_on_signal):
-		dialogic.text_signal.disconnect(_call_on_signal)
+	finish()
 
 
 ################################################################################
@@ -73,7 +54,7 @@ func _disconnect_signal():
 ################################################################################
 
 func _init() -> void:
-	event_name = "Call Node"
+	event_name = "Call"
 	set_default_color('Color6')
 	event_category = "Logic"
 	event_sorting_index = 10
@@ -83,20 +64,58 @@ func _init() -> void:
 ## 						SAVING/LOADING
 ################################################################################
 
-func get_shortcode() -> String:
-	return "call_node"
+func to_text() -> String:
+	var result := "do "
+	if autoload_name:
+		result += autoload_name
+		if method:
+			result += '.'+method
+			if arguments.is_empty():
+				result += '()'
+			else:
+				result += '('
+				var arr := []
+				for i in arguments:
+					if i is String and i.begins_with('@'):
+						result += i.trim_prefix('@')
+					else:
+						result += var_to_str(i)
+					result += ', '
+				result = result.trim_suffix(', ')+')'
+	return result
+
+
+func from_text(string:String) -> void:
+	var result := RegEx.create_from_string('do (?<autoload>[^\\.]*)(.(?<method>[^.(]*)(\\((?<arguments>.*)\\))?)?').search(string.strip_edges())
+	if result:
+		autoload_name = result.get_string('autoload')
+		method = result.get_string('method')
+		if result.get_string('arguments').is_empty():
+			arguments = []
+		else:
+			var arr := []
+			for i in result.get_string('arguments').split(','):
+				i = i.strip_edges()
+				if str_to_var(i) != null:
+					arr.append(str_to_var(i))
+				else:
+					# Mark this as a complex expression
+					arr.append("@"+i)
+			arguments = arr
+
+
+func is_valid_event(string:String) -> bool:
+	if string.strip_edges().begins_with("do"):
+		return true
+	return false
 
 
 func get_shortcode_parameters() -> Dictionary:
 	return {
 		#param_name : property_info
-		"path" 		: {"property": "path", 		"default": ""},
-		"method" 	: {"property": "method", 	"default": ""},
-		"args" 		: {"property": "arguments", "default": []},
-		"wait" 		: {"property": "wait", 		"default": false},
-		"inline" 	: {"property": "inline", 	"default": false},
-		"signal" 	: {"property": "inline_signal_argument", 	"default": ""},
-		"single_use": {"property": "inline_single_use", 		"default": true}
+		"autoload" 	: {"property": "autoload_name", 	"default": ""},
+		"method" 	: {"property": "method", 			"default": ""},
+		"args" 		: {"property": "arguments", 		"default": []},
 	}
 
 
@@ -105,11 +124,107 @@ func get_shortcode_parameters() -> Dictionary:
 ################################################################################
 
 func build_event_editor():
-	add_header_edit('method', ValueType.SINGLELINE_TEXT, {'left_text':'Call method'})
-	add_header_edit('path', ValueType.SINGLELINE_TEXT, {'left_text':'in object'})
-	add_body_edit('inline', ValueType.BOOL, {'left_text':'Inline Command:', 'tooltip':"If enabled, the method won't be called instantly. Only when a signal is emmited inside the following text event will it be called."})
-	add_body_edit('inline_signal_argument', ValueType.SINGLELINE_TEXT, {'left_text':'Inline Signal Argument', 'tooltip':"For example if set to 'Hello' the method can be called with [signal=Hello] in the next text event."}, 'inline == true')
-	add_body_edit('inline_single_use', ValueType.BOOL, {'left_text':'Single Use:','tooltip':"By default calling via in-text signal only works once. Uncheck this to make the event keep listening. \nThis only stays valid during this dialog."}, 'inline == true')
-	add_body_edit('wait', ValueType.BOOL, {'left_text':'Wait:', 'tooltip':'Will wait for the method to finish. Only relevant for methods with `await` in them.'}, 'inline == false')
-	add_body_line_break()
-	add_body_edit('arguments', ValueType.STRING_ARRAY, {'left_text':'Arguments:'})
+	add_header_edit('autoload_name', ValueType.COMPLEX_PICKER, {'left_text':'On autoload',
+		'empty_text':'Autoload',
+		'suggestions_func':get_autoload_suggestions,
+		'editor_icon':["Node", "EditorIcons"]})
+	add_header_edit('method', ValueType.COMPLEX_PICKER, {'left_text':'call',
+		'empty_text':'Method',
+		'suggestions_func':get_method_suggestions,
+		'editor_icon':["Callable", "EditorIcons"]}, 'autoload_name')
+	add_body_edit('arguments', ValueType.STRING_ARRAY, {'left_text':'Arguments:'}, 'not autoload_name.is_empty() and not method.is_empty()')
+
+
+
+func get_autoload_suggestions(filter:String="") -> Dictionary:
+	var suggestions := {}
+
+	for prop in ProjectSettings.get_property_list():
+		if prop.name.begins_with('autoload/'):
+			var autoload: String = prop.name.trim_prefix('autoload/')
+			suggestions[autoload] = {'value': autoload, 'tooltip':autoload, 'editor_icon': ["Node", "EditorIcons"]}
+	return suggestions
+
+
+func get_method_suggestions(filter:String="", temp_autoload:String = "") -> Dictionary:
+	var suggestions := {}
+
+	var script : Script
+	if temp_autoload:
+		script = load(ProjectSettings.get_setting('autoload/'+temp_autoload).trim_prefix('*'))
+	elif autoload_name:
+		script = load(ProjectSettings.get_setting('autoload/'+autoload_name).trim_prefix('*'))
+	if script:
+		for method in script.get_script_method_list():
+			if method.name.begins_with('@') or method.name.begins_with('_'):
+				continue
+			suggestions[method.name] = {'value': method.name, 'tooltip':method.name, 'editor_icon': ["Callable", "EditorIcons"]}
+	return suggestions
+
+
+func update_argument_info() -> void:
+	if autoload_name and method and not (_current_method_arg_hints.a == autoload_name and _current_method_arg_hints.m == method):
+		var script :Script = load(ProjectSettings.get_setting('autoload/'+autoload_name).trim_prefix('*'))
+		for m in script.get_script_method_list():
+			if m.name == method:
+				_current_method_arg_hints = {'a':autoload_name, 'm':method, 'info':m}
+				print(m)
+				break
+
+
+func check_arguments_and_update_warning():
+	if _current_method_arg_hints.info.is_empty():
+		ui_update_warning.emit()
+		return
+
+	var idx := -1
+	for arg in arguments:
+		idx += 1
+		if _current_method_arg_hints.info.args[idx].type != 0:
+			if _current_method_arg_hints.info.args[idx].type != typeof(arg):
+				if arg is String and arg.begins_with('@'):
+					continue
+				var expected_type :String = ""
+				match _current_method_arg_hints.info.args[idx].type:
+					TYPE_BOOL: 		expected_type = "bool"
+					TYPE_STRING: 	expected_type = "string"
+					TYPE_FLOAT: 	expected_type = "float"
+					TYPE_INT: 		expected_type = "int"
+					_: 				expected_type = "something else"
+
+				ui_update_warning.emit('Argument '+ str(idx+1)+ ' ('+_current_method_arg_hints.info.args[idx].name+') has the wrong type (method expects '+expected_type+')!')
+				return
+
+	if len(arguments) < len(_current_method_arg_hints.info.args)-len(_current_method_arg_hints.info.default_args):
+		ui_update_warning.emit("The method is expecting at least "+str(len(_current_method_arg_hints.info.args)-len(_current_method_arg_hints.info.default_args))+ " arguments, but is given only "+str(len(arguments))+".")
+		return
+	ui_update_warning.emit()
+
+####################### CODE COMPLETION ########################################
+################################################################################
+
+func _get_code_completion(CodeCompletionHelper:Node, TextNode:TextEdit, line:String, word:String, symbol:String) -> void:
+	if line.count(' ') == 1 and not '.' in line:
+		for i in get_autoload_suggestions():
+			TextNode.add_code_completion_option(CodeEdit.KIND_MEMBER, i, i+'.', event_color.lerp(TextNode.syntax_highlighter.normal_color, 0.3), TextNode.get_theme_icon("Node", "EditorIcons"))
+	elif symbol == '.' and not '(' in line:
+		for i in get_method_suggestions('', line.get_slice('.', 0).trim_prefix('do ')):
+			TextNode.add_code_completion_option(CodeEdit.KIND_MEMBER, i, i+'(', event_color.lerp(TextNode.syntax_highlighter.normal_color, 0.3), TextNode.get_theme_icon("Callable", "EditorIcons"))
+
+
+func _get_start_code_completion(CodeCompletionHelper:Node, TextNode:TextEdit) -> void:
+	TextNode.add_code_completion_option(CodeEdit.KIND_PLAIN_TEXT, 'do', 'do ', event_color.lerp(TextNode.syntax_highlighter.normal_color, 0.3), _get_icon())
+
+
+#################### SYNTAX HIGHLIGHTING #######################################
+################################################################################
+
+func _get_syntax_highlighting(Highlighter:SyntaxHighlighter, dict:Dictionary, line:String) -> Dictionary:
+	dict[line.find('do')] = {"color":event_color.lerp(Highlighter.normal_color, 0.3)}
+	dict[line.find('do')+2] = {"color":event_color.lerp(Highlighter.normal_color, 0.5)}
+
+	Highlighter.color_region(dict, Highlighter.normal_color, line, '(', ')')
+	Highlighter.color_region(dict, Highlighter.string_color, line, '"', '"')
+	Highlighter.color_word(dict, Highlighter.boolean_operator_color, line, 'true')
+	Highlighter.color_word(dict, Highlighter.boolean_operator_color, line, 'false')
+	return dict
