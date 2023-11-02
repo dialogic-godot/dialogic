aa5c82cd807b95de3db51e0cb37714f735faf7e0
Jowan-Spooner <raban-loeffler@posteo.de>
2023-11-02 18:29:30 +0100

    Remove input handler, add doc comment

--- addons/dialogic/Modules/Text/node_input.gd
+++ addons/dialogic/Modules/Text/node_input.gd
@@ -1,8 +1,10 @@
 class_name DialogicNode_Input
 extends Control
 
+## A node that handles mouse input. This allows limiting mouse input to a
+## specific region and avoiding conflicts with other UI elements.
+## If no Input node is used, the input subsystem will handle mouse input instead.
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	add_to_group('dialogic_input')
 	gui_input.connect(_on_gui_input)
