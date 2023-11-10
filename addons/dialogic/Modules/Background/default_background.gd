c466699ba24bcba32e61fcfe40f147354f1a1b95
Pheubel <luca.guagliardo@live.nl>
2023-11-10 01:35:11 -0800

    Render backgrounds in sub-viewports and transition them using shaders (#1839)
    This keeps all the new viewport and shader stuff but offloads most of the work to behind the scenes. This means background scenes can work just as they did previously. However they are manually put into subvieports+subviewportcontainers. The backgrounds-holder is now also the background-displayer.
    
    _fade_out() and _fade_in() have been renamed to _custom_fade_out() and _custom_fade_in(). Right now they work "additionally" to the default fade which happens as well. If you know what you are doing you can work around this though.
    I have to put further thought into custom transitions as I have no real idea what the best user-experience/use case might be.
    
    ---------
    
    Co-authored-by: Jowan-Spooner <raban-loeffler@posteo.de>

--- addons/dialogic/Modules/Background/default_background.gd
+++ addons/dialogic/Modules/Background/default_background.gd
@@ -1,27 +1,30 @@
 extends DialogicBackground
 
-## The default background scene. 
+## The default background scene.
 ## Extend the DialogicBackground class to create your own background scene.
+
+@onready var image_node = $Image
+@onready var color_node = $ColorRect
 
 
 func _ready() -> void:
-	$Image.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
-	$Image.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_COVERED
-	
-	$Image.anchor_right = 1
-	$Image.anchor_bottom = 1
+	image_node.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+	image_node.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_COVERED
+
+	image_node.anchor_right = 1
+	image_node.anchor_bottom = 1
 
 
 func _update_background(argument:String, time:float) -> void:
 	if argument.begins_with('res://'):
-		$Image.texture = load(argument)
-		self.color = Color.TRANSPARENT
+		image_node.texture = load(argument)
+		color_node.color = Color.TRANSPARENT
 	elif argument.is_valid_html_color():
-		$Image.texture = null
-		self.color = Color(argument, 1)
+		image_node.texture = null
+		color_node.color = Color(argument, 1)
 	else:
-		$Image.texture = null
-		self.color = Color.from_string(argument, Color.TRANSPARENT)
+		image_node.texture = null
+		color_node.color = Color.from_string(argument, Color.TRANSPARENT)
 
 
 func _should_do_background_update(argument:String) -> bool:
