a7f565cc706c4a52a65a322304b1bbf834dbb32d
Pheubel <luca.guagliardo@live.nl>
2023-11-10 01:35:11 -0800

    Render backgrounds in sub-viewports and transition them using shaders (#1839)
    This keeps all the new viewport and shader stuff but offloads most of the work to behind the scenes. This means background scenes can work just as they did previously. However they are manually put into subvieports+subviewportcontainers. The backgrounds-holder is now also the background-displayer.
    
    _fade_out() and _fade_in() have been renamed to _custom_fade_out() and _custom_fade_in(). Right now they work "additionally" to the default fade which happens as well. If you know what you are doing you can work around this though.
    I have to put further thought into custom transitions as I have no real idea what the best user-experience/use case might be.
    
    ---------
    
    Co-authored-by: Jowan-Spooner <raban-loeffler@posteo.de>

--- addons/dialogic/Modules/Background/node_background_holder.gd
+++ addons/dialogic/Modules/Background/node_background_holder.gd
@@ -1,6 +1,8 @@
 class_name DialogicNode_BackgroundHolder
-extends CanvasLayer
+extends ColorRect
 
 
 func _ready():
 	add_to_group('dialogic_background_holders')
+	if material == null:
+		material = load("res://addons/dialogic/Modules/Background/default_background_transition.tres")
