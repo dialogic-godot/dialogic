77948f8275e7ac2a7c9742f90a5735154a778d31
Jowan-Spooner <42868150+Jowan-Spooner@users.noreply.github.com>
2023-12-05 18:44:48 +0100

    Speaker highlight improvements (#1941)
    * Fix simple-string portraits
    
    * Fix _highlight & _unhighlight and make custom portrait easier
    
    This offloads a lot of the work for simple Sprite and TextureRect based portraits a lot easier by having helper methods on the portrait do that work.
    This will allow us to make more variations of these kinds of portraits very easily.
    
    * Example Custom Portrait with Highlighting
    
    Also small jump suggestions change.

---
+++ addons/dialogic/Example Assets/portraits/simple_highlight_portrait.gd
@@ -0,0 +1,29 @@
+@tool
+extends DialogicPortrait
+
+@export_group('Main')
+@export_file var image : String = ""
+
+var unhighlighted_color := Color.DARK_GRAY
+var prev_z_index := 0
+
+## Load anything related to the given character and portrait
+func _update_portrait(passed_character:DialogicCharacter, passed_portrait:String) -> void:
+	apply_character_and_portrait(passed_character, passed_portrait)
+
+	apply_texture($Portrait, image)
+
+
+func _ready() -> void:
+	self.modulate = unhighlighted_color
+
+
+func _highlight():
+	create_tween().tween_property(self, 'modulate', Color.WHITE, 0.15)
+	prev_z_index = Dialogic.Portraits.get_character_info(character).get('z_index', 0)
+	Dialogic.Portraits.change_character_z_index(character, 10)
+
+
+func _unhighlight():
+	create_tween().tween_property(self, 'modulate', unhighlighted_color, 0.15)
+	Dialogic.Portraits.change_character_z_index(character, prev_z_index)
