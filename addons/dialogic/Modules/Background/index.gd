01891108f678062ff16a8d4ed94a5235eac57e34
Pheubel <luca.guagliardo@live.nl>
2023-12-06 22:15:13 +0100

    added rough editor for transitions

--- addons/dialogic/Modules/Background/index.gd
+++ addons/dialogic/Modules/Background/index.gd
@@ -5,6 +5,8 @@ extends DialogicIndexer
 func _get_events() -> Array:
 	return [this_folder.path_join('event_background.gd')]
 
+func _get_editors() -> Array:
+	return [this_folder.path_join('transition_editor.tscn')]
 
 func _get_subsystems() -> Array:
 	return [{'name':'Backgrounds', 'script':this_folder.path_join('subsystem_backgrounds.gd')}]
