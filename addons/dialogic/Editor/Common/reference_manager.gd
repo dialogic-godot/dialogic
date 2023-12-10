126974e08d362459a57493388a8478f9b8405ad8
Jowan-Spooner <raban-loeffler@posteo.de>
2023-12-10 16:44:41 +0100

    Fixes & Improvements to ReferenceManger

--- addons/dialogic/Editor/Common/reference_manager.gd
+++ addons/dialogic/Editor/Common/reference_manager.gd
@@ -11,3 +11,7 @@ func _ready() -> void:
 	$Tabs.add_theme_color_override("font_selected_color", get_theme_color("accent_color", "Editor"))
 	$Tabs.add_theme_font_override("font", get_theme_font("main_button_font", "EditorFonts"))
 
+
+func open():
+	show()
+	$Tabs/BrokenReferences.update_indicator()
