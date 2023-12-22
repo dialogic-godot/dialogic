2afe4957a3ac7d19355892550bfa86552673dea0
Jowan-Spooner <raban-loeffler@posteo.de>
2023-12-22 11:29:22 +0100

    Remove bad Dialogic reference

--- addons/dialogic/Modules/Background/subsystem_backgrounds.gd
+++ addons/dialogic/Modules/Background/subsystem_backgrounds.gd
@@ -38,7 +38,7 @@ func load_game_state(load_flag:=LoadFlags.FULL_LOAD):
 func update_background(scene:String = '', argument:String = '', fade_time:float = 0.0, transition_path:=default_transition, force:bool = false) -> void:
 	var background_holder: DialogicNode_BackgroundHolder
 	if dialogic.has_subsystem('Styles'):
-		background_holder = Dialogic.Styles.get_first_node_in_layout('dialogic_background_holders')
+		background_holder = dialogic.Styles.get_first_node_in_layout('dialogic_background_holders')
 	else:
 		background_holder = get_tree().get_first_node_in_group('dialogic_background_holders')
 	if background_holder == null:
