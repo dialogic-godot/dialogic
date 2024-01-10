aa3877fe1069181bbe0c382417eea6faea16487b
Miisan <128460054+psy-lilulu@users.noreply.github.com>
2024-01-10 21:24:15 +0800

    Refactor plugin.gd for improved readability and maintainability
    This commit includes several changes to improve the readability and maintainability of the plugin.gd file:
    
    1. More descriptive comments have been added to provide better context for the code.
    2. Repeated code checking the existence of `editor_view` and `editor_view.editors_manager` has been moved into a helper function `_editor_view_and_manager_exist()`.
    3. String literals that were used multiple times in the code have been replaced with constants to avoid magic strings and make future changes easier.

--- addons/dialogic/plugin.gd
+++ addons/dialogic/plugin.gd
@@ -1,123 +1,99 @@
 @tool
 extends EditorPlugin
 
-## Main plugin script. Handles communication with the rest of godot.
-## Most methods are overridables called by godot.
-
-
+# Preload the main panel scene
 const MainPanel := preload("res://addons/dialogic/Editor/editor_main.tscn")
+const PLUGIN_NAME = "Dialogic"
+const PLUGIN_HANDLER_PATH = "res://addons/dialogic/Other/DialogicGameHandler.gd"
+const PLUGIN_ICON_PATH = "res://addons/dialogic/Editor/Images/plugin-icon.svg"
 
-### References
-# used by various other scripts to quickly reference these things
+# References used by various other scripts to quickly reference these things
+var editor_view: Control  # the root of the dialogic editor
 
-# the root of the dialogic editor
-var editor_view: Control
-
-# emitted if godot wants us to save
+# Signal emitted if godot wants us to save
 signal dialogic_save
 
-
-################################################################################
-## 					INITIALIZING
-################################################################################
-
+# Initialization
 func _init() -> void:
-	self.name = 'DialogicPlugin'
+    self.name = PLUGIN_NAME
 
-
-################################################################################
-## 					ACTIVATION & EDITOR SETUP
-################################################################################
-
+# Activation & Editor Setup
 func _enable_plugin():
-	add_autoload_singleton("Dialogic", "res://addons/dialogic/Other/DialogicGameHandler.gd")
-	add_dialogic_default_action()
-
+    add_autoload_singleton(PLUGIN_NAME, PLUGIN_HANDLER_PATH)
+    add_dialogic_default_action()
 
 func _disable_plugin():
-	remove_autoload_singleton("Dialogic")
-
+    remove_autoload_singleton(PLUGIN_NAME)
 
 func _enter_tree() -> void:
-	editor_view = MainPanel.instantiate()
-	editor_view.plugin_reference = self
-	editor_view.hide()
-	get_editor_interface().get_editor_main_screen().add_child(editor_view)
-	_make_visible(false)
-
+    editor_view = MainPanel.instantiate()
+    editor_view.plugin_reference = self
+    editor_view.hide()
+    get_editor_interface().get_editor_main_screen().add_child(editor_view)
+    _make_visible(false)
 
 func _exit_tree() -> void:
-	if editor_view:
-		remove_control_from_bottom_panel(editor_view)
-		editor_view.queue_free()
+    if editor_view:
+        remove_control_from_bottom_panel(editor_view)
+        editor_view.queue_free()
 
-
+# Plugin Info
 func _has_main_screen() -> bool:
-	return true
-
+    return true
 
 func _get_plugin_name() -> String:
-	return "Dialogic"
-
+    return PLUGIN_NAME
 
 func _get_plugin_icon():
-	return load("res://addons/dialogic/Editor/Images/plugin-icon.svg")
+    return load(PLUGIN_ICON_PATH)
 
-
-################################################################################
-## 					EDITOR INTERACTION
-################################################################################
-
+# Editor Interaction
 func _make_visible(visible:bool) -> void:
-	if editor_view:
-		if editor_view.get_parent() is Window:
-			if visible:
-				get_editor_interface().set_main_screen_editor("Script")
-				editor_view.show()
-				editor_view.get_parent().grab_focus()
-		else:
-			editor_view.visible = visible
-
+    if editor_view:
+        if editor_view.get_parent() is Window:
+            if visible:
+                get_editor_interface().set_main_screen_editor("Script")
+                editor_view.show()
+                editor_view.get_parent().grab_focus()
+        else:
+            editor_view.visible = visible
 
 func _save_external_data() -> void:
-	if editor_view and editor_view.editors_manager:
-		editor_view.editors_manager.save_current_resource()
-
+    if _editor_view_and_manager_exist():
+        editor_view.editors_manager.save_current_resource()
 
 func _handles(object) -> bool:
-	if editor_view != null and editor_view.editors_manager != null and object is Resource:
-		return editor_view.editors_manager.can_edit_resource(object)
-	return false
-
+    if _editor_view_and_manager_exist() and object is Resource:
+        return editor_view.editors_manager.can_edit_resource(object)
+    return false
 
 func _edit(object) -> void:
-	if object == null:
-		return
-	_make_visible(true)
-	if editor_view and editor_view.editors_manager:
-		editor_view.editors_manager.edit_resource(object)
+    if object == null:
+        return
+    _make_visible(true)
+    if _editor_view_and_manager_exist():
+        editor_view.editors_manager.edit_resource(object)
 
-
-
-################################################################################
-## 					SPECIAL SETUP/UPDATES
-################################################################################
-
-# methods that adds a dialogic_default_action if non exists
+# Special Setup/Updates
+# Methods that adds a dialogic_default_action if non exists
 func add_dialogic_default_action() -> void:
-	if !ProjectSettings.has_setting('input/dialogic_default_action'):
-		var input_enter : InputEventKey = InputEventKey.new()
-		input_enter.keycode = KEY_ENTER
-		var input_left_click : InputEventMouseButton = InputEventMouseButton.new()
-		input_left_click.button_index = MOUSE_BUTTON_LEFT
-		input_left_click.pressed = true
-		input_left_click.device = -1
-		var input_space : InputEventKey = InputEventKey.new()
-		input_space.keycode = KEY_SPACE
-		var input_x : InputEventKey = InputEventKey.new()
-		input_x.keycode = KEY_X
-		var input_controller : InputEventJoypadButton = InputEventJoypadButton.new()
-		input_controller.button_index = JOY_BUTTON_A
+    if !ProjectSettings.has_setting('input/dialogic_default_action'):
+        var input_enter : InputEventKey = InputEventKey.new()
+        input_enter.keycode = KEY_ENTER
+        var input_left_click : InputEventMouseButton = InputEventMouseButton.new()
+        input_left_click.button_index = MOUSE_BUTTON_LEFT
+        input_left_click.pressed = true
+        input_left_click.device = -1
+        var input_space : InputEventKey = InputEventKey.new()
+        input_space.keycode = KEY_SPACE
+        var input_x : InputEventKey = InputEventKey.new()
+        input_x.keycode = KEY_X
+        var input_controller : InputEventJoypadButton = InputEventJoypadButton.new()
+        input_controller.button_index = JOY_BUTTON_A
 
-		ProjectSettings.set_setting('input/dialogic_default_action', {'deadzone':0.5, 'events':[input_enter, input_left_click, input_space, input_x, input_controller]})
-		ProjectSettings.save()
+        ProjectSettings.set_setting('input/dialogic_default_action', {'deadzone':0.5, 'events':[input_enter, input_left_click, input_space, input_x, input_controller]})
+        ProjectSettings.save()
+
+# Helper function to check if editor_view and its manager exist
+func _editor_view_and_manager_exist() -> bool:
+    return editor_view and editor_view.editors_manager
