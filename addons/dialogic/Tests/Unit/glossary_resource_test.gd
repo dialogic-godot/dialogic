7766f158d66ac0f4b783ea3810518efe41ab9a9a
Cake <cake@cakey.moe>
2024-01-11 11:11:51 +0100

    Add test for invalid entry key indexes.

--- addons/dialogic/Tests/Unit/glossary_resource_test.gd
+++ addons/dialogic/Tests/Unit/glossary_resource_test.gd
@@ -14,7 +14,7 @@ func test_add_entry() -> void:
 	const EXPECTED_ENTRY_INDEX := 0
 
 	var new_entry := {
-		"title": EXAMPLE_TITLE,
+		DialogicGlossary.TITLE_PROPERTY: EXAMPLE_TITLE,
 		DialogicGlossary.NAME_PROPERTY: NAME_ENTRY,
 		DialogicGlossary.ALTERNATIVE_PROPERTY: ALTERNATIVE_ENTRIES
 	}
@@ -47,7 +47,7 @@ func test_replace_entries() -> void:
 	const MISSING_INDEX := -1
 
 	var new_entry := {
-		"title": EXAMPLE_TITLE,
+		DialogicGlossary.TITLE_PROPERTY: EXAMPLE_TITLE,
 		DialogicGlossary.NAME_PROPERTY: NAME_ENTRY,
 		DialogicGlossary.ALTERNATIVE_PROPERTY: ALTERNATIVE_ENTRIES
 	}
@@ -66,3 +66,39 @@ func test_replace_entries() -> void:
 	assert(old_entry_index == MISSING_INDEX, "Old entry should not be found, entry index was: " + str(old_entry_index) + ".")
 
 
+## We are testing if a glossary with an entry key leading to a wrong index
+## will be corrected.
+## The correction must find the matching entry and take its index as the
+## proper corrected index.
+## This test stems from a bug, where editing the glossary in the editor
+## was not updating the key properly.
+func test_invalid_entry_index_correction() -> void:
+	var glossary: DialogicGlossary = DialogicGlossary.new()
+	const EXAMPLE_NAME := "Example Name"
+	const EXAMPLE_TITLE := "Example Title"
+	const ALTERNATIVE_ENTRIES := ["A", "BE", "VERY LONG ENTRY"]
+
+	const EXPECTED_INDEX := 0
+
+	var new_entry := {
+		DialogicGlossary.TITLE_PROPERTY: EXAMPLE_TITLE,
+		DialogicGlossary.NAME_PROPERTY: EXAMPLE_NAME,
+		DialogicGlossary.ALTERNATIVE_PROPERTY: ALTERNATIVE_ENTRIES
+	}
+
+	# Initially, we add the entry.
+	glossary.set_entry(EXAMPLE_NAME, new_entry)
+
+	# Verify the glossary has properly picked up the entry.
+	var entry_index: int = glossary._find_entry_index_by_key(EXAMPLE_NAME)
+	assert(entry_index == EXPECTED_INDEX, "Entry index should be " + str(EXPECTED_INDEX) + ", was: " + str(entry_index) + ".")
+
+	# Now, we will forcefully invalidate the entry key.
+	glossary._entry_keys[0] = 2000
+
+	# Next, we will try to rename the entry.
+	const NEW_NAME := "new name"
+	glossary.replace_entry_key(EXAMPLE_NAME, NEW_NAME)
+
+	var corrected_entry_index: int = glossary._find_entry_index_by_key(NEW_NAME)
+	assert(corrected_entry_index == EXPECTED_INDEX, "Corrected entry index should be " + str(EXPECTED_INDEX) + ", was: " + str(corrected_entry_index) + ".")
