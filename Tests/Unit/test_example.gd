46c76a76ebf427c4593706ead12d1ef002200f99
Cake <65981767+CakeVR@users.noreply.github.com>
2024-01-20 12:53:32 +0100

    Move Unit Tests outside of Dialogic (#2030)
    * Move unit tests outside of Dialogic.
    
    * Move more tests.

--- Tests/Unit/test_example.gd
+++ Tests/Unit/test_example.gd
@@ -1,7 +1,8 @@
+class_name GdUnitExampleTest
 extends GdUnitTestSuite
 
 func test_example() -> void:
-    const EXAMPLE_STRING := "Dialogic Test!"
+    const EXAMPLE_STRING := "Dialogic!"
 
     assert_str(EXAMPLE_STRING)\
         .has_length(EXAMPLE_STRING.length())\
