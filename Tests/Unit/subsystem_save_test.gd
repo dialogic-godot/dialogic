ce369a7d2802872fc61643226d891c1c13b311ff
Cake <cake@cakey.moe>
2024-02-10 10:13:16 +0100

    Add method to get the global data of the seen events itself.

---
+++ Tests/Unit/subsystem_save_test.gd
@@ -0,0 +1,44 @@
+extends GdUnitTestSuite
+
+var history := Dialogic.History
+var save := Dialogic.Save
+
+const EXAMPLE_SEEN_HISTORY: Dictionary = { "res://Dialogic/Timelines/start.dtl1": 1,
+    "res://Dialogic/Timelines/start.dtl2": 2,
+    "res://Dialogic/Timelines/start.dtl3": 3,
+    "res://Dialogic/Timelines/start.dtl4": 4,
+    "res://Dialogic/Timelines/start.dtl5": 5,
+    "res://Dialogic/Timelines/start.dtl7": 7
+}
+
+func test_save_already_seen() -> void:
+    assert(history.already_read_history_content == {}, "Seen events should have be empty.")
+    save.load_already_seen_history()
+    assert(history.already_read_history_content == {}, "Seen events should have be empty after empty load.")
+
+    history.already_read_history_content = EXAMPLE_SEEN_HISTORY
+    assert(history.already_read_history_content == EXAMPLE_SEEN_HISTORY, "Seen events should have be identical to test data.")
+
+    save.save_already_seen_history()
+    var global_data_seen_events: Dictionary = save.get_global_info("already_seen_history", {})
+
+    assert(global_data_seen_events == EXAMPLE_SEEN_HISTORY, "Global data does not have example data.")
+
+    save.load_already_seen_history()
+    assert(history.already_read_history_content == EXAMPLE_SEEN_HISTORY, "Seen events should have be identical to test data after load.")
+
+
+func test_deletion_save_already_seen() -> void:
+    history.already_read_history_content = EXAMPLE_SEEN_HISTORY
+    assert(history.already_read_history_content == EXAMPLE_SEEN_HISTORY, "Seen events should have be identical to test data.")
+
+    save.save_already_seen_history()
+    var global_data_seen_events: Dictionary = save.get_global_info("already_seen_history", {})
+    assert(global_data_seen_events == EXAMPLE_SEEN_HISTORY, "Global data does not have example data.")
+
+    save.reset_already_seen_history(false)
+    assert(history.already_read_history_content == EXAMPLE_SEEN_HISTORY, "Seen events are gone after global data only reset.")
+    assert(save.get_global_info("already_seen_history", {}) == {}, "Global data should be empty after reset.")
+
+    save.load_already_seen_history()
+    assert(history.already_read_history_content == {}, "Seen events should have be empty after empty load.")
