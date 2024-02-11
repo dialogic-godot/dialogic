0ad77880fa9f3c3ef8ed0bdd361c638908f54d80
Cake <cake@cakey.moe>
2024-02-11 19:37:32 +0100

    Fix more methods on the wrong subsystem.

--- Tests/Unit/subsystem_save_test.gd
+++ Tests/Unit/subsystem_save_test.gd
@@ -25,7 +25,7 @@ func test_save_load_already_seen() -> void:
 
     assert(global_data_seen_events == EXAMPLE_SEEN_HISTORY, "Global data does not have example data.")
 
-    save.load_already_seen_history()
+    history.load_already_seen_history()
     assert(history.already_read_history_content == EXAMPLE_SEEN_HISTORY, "Seen events should have be identical to test data after load.")
 
 
@@ -37,16 +37,16 @@ func test_deletion_save_already_seen() -> void:
     var global_data_seen_events: Dictionary = save.get_saved_already_seen_history()
     assert(global_data_seen_events == EXAMPLE_SEEN_HISTORY, "Global data does not have example data.")
 
-    save.reset_already_seen_history(false)
+    history.reset_already_seen_history(false)
     var global_data_seen_events_after_reset: Dictionary = save.get_saved_already_seen_history()
     assert(history.already_read_history_content == EXAMPLE_SEEN_HISTORY, "Seen events are gone after global data only reset.")
     assert(global_data_seen_events_after_reset == {}, "Global data should be empty after reset.")
 
-    save.load_already_seen_history()
+    history.load_already_seen_history()
     assert(history.already_read_history_content == {}, "Seen events should have be empty after empty load.")
 
     history.already_read_history_content = EXAMPLE_SEEN_HISTORY
-    save.reset_already_seen_history(true)
+    history.reset_already_seen_history(true)
     var global_data_seen_events_after_full_reset: Dictionary = history.get_saved_already_seen_history()
     assert(global_data_seen_events_after_full_reset == {}, "Seen events in global data should have be empty after full data reset.")
     assert(history.already_read_history_content == {}, "Seen events in history should have be empty after full data reset.")
