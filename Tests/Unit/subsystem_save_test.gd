7f25765604d8e2e625a49beb4abb461ad89c048b
Cake <cake@cakey.moe>
2024-02-11 19:08:10 +0100

    Fix method name in unit test.

--- Tests/Unit/subsystem_save_test.gd
+++ Tests/Unit/subsystem_save_test.gd
@@ -21,7 +21,7 @@ func test_save_load_already_seen() -> void:
     assert(history.already_read_history_content == EXAMPLE_SEEN_HISTORY, "Seen events should have be identical to test data.")
 
     save.save_already_seen_history()
-    var global_data_seen_events: Dictionary = save.get_already_seen_history()
+    var global_data_seen_events: Dictionary = save.get_saved_already_seen_history()
 
     assert(global_data_seen_events == EXAMPLE_SEEN_HISTORY, "Global data does not have example data.")
 
@@ -34,11 +34,11 @@ func test_deletion_save_already_seen() -> void:
     assert(history.already_read_history_content == EXAMPLE_SEEN_HISTORY, "Seen events should have be identical to test data.")
 
     save.save_already_seen_history()
-    var global_data_seen_events: Dictionary = save.get_already_seen_history()
+    var global_data_seen_events: Dictionary = save.get_saved_already_seen_history()
     assert(global_data_seen_events == EXAMPLE_SEEN_HISTORY, "Global data does not have example data.")
 
     save.reset_already_seen_history(false)
-    var global_data_seen_events_after_reset: Dictionary = save.get_already_seen_history()
+    var global_data_seen_events_after_reset: Dictionary = save.get_saved_already_seen_history()
     assert(history.already_read_history_content == EXAMPLE_SEEN_HISTORY, "Seen events are gone after global data only reset.")
     assert(global_data_seen_events_after_reset == {}, "Global data should be empty after reset.")
 
@@ -47,6 +47,6 @@ func test_deletion_save_already_seen() -> void:
 
     history.already_read_history_content = EXAMPLE_SEEN_HISTORY
     save.reset_already_seen_history(true)
-    var global_data_seen_events_after_full_reset: Dictionary = save.get_already_seen_history()
+    var global_data_seen_events_after_full_reset: Dictionary = history.get_saved_already_seen_history()
     assert(global_data_seen_events_after_full_reset == {}, "Seen events in global data should have be empty after full data reset.")
     assert(history.already_read_history_content == {}, "Seen events in history should have be empty after full data reset.")
