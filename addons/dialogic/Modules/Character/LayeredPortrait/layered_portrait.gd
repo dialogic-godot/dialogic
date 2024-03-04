e8d8a115ab01ceeaa4828081de1cffa3a7560c55
Cake <cake@cakey.moe>
2024-03-04 11:31:41 +0100

    Fix portrait updating.

--- addons/dialogic/Modules/Character/LayeredPortrait/layered_portrait.gd
+++ addons/dialogic/Modules/Character/LayeredPortrait/layered_portrait.gd
@@ -13,10 +13,21 @@ static var OPERATORS_EXPRESSION := "|".join(OPERATORS)
 static var REGEX_STRING := "(" + OPERATORS_EXPRESSION + ") (\\S+)"
 static var REGEX := RegEx.create_from_string(REGEX_STRING)
 
+var _initialized := false
 
 ## Load anything related to the given character and portrait
 func _update_portrait(passed_character: DialogicCharacter, passed_portrait: String) -> void:
+
+	if not _initialized:
+		for sprite: Sprite2D in _find_sprites_recursively(self):
+			sprite.scale = Vector2.ONE
+			sprite.centered = false
+			sprite.position = (sprite.get_rect().size * Vector2(-0.5, -1)) + sprite.position
+
+		_initialized = true
+
 	apply_character_and_portrait(passed_character, passed_portrait)
+
 
 ## Iterates over all children in [param start_node] and its children, looking
 ## for [class Sprite2D] nodes with a texture (not `null`).
@@ -24,24 +35,16 @@ func _find_sprites_recursively(start_node: Node) -> Array[Sprite2D]:
 	var sprites: Array[Sprite2D] = []
 
 	# Iterate through the children of the current node
-	for child in start_node.get_children():
+	for child: Node in start_node.get_children():
 
 		if child is Sprite2D and child.texture:
-			var sprite := child as Sprite2D
-			sprites.append(sprite)
+			sprites.append(child)
 			continue
 
 		var child_sprites := _find_sprites_recursively(child)
 		sprites.append_array(child_sprites)
 
 	return sprites
-
-
-func _ready() -> void:
-	for sprite: Sprite2D in _find_sprites_recursively(self):
-		sprite.scale = Vector2.ONE
-		sprite.centered = false
-		sprite.position = (sprite.get_rect().size * Vector2(-0.5, -1)) + sprite.position
 
 
 ## A _command that will apply an effect to the layered portrait.
@@ -64,20 +67,26 @@ class LayerCommand:
 			print("Layered Portrait had no node matching the node path: ", _path)
 			return
 
+		if target_node is Sprite2D:
+			print("Layered Portrait target path '", _path, "', is not a Sprite2D node.")
+			return
+
+		var sprite := target_node as Sprite2D
+
 		match _type:
 			CommandType.SHOW_LAYER:
-				target_node.show()
+				sprite.show()
 
 			CommandType.HIDE_LAYER:
-				target_node.hide()
+				sprite.hide()
 
 			CommandType.SET_LAYER:
 				var target_parent := target_node.get_parent()
 
 				for child in target_parent.get_children():
-					child.hide()
+					sprite.hide()
 
-				target_node.show()
+				sprite.show()
 
 
 ## Turns the input into a single [class LayerCommand] object.
