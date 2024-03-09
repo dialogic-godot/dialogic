e743897d95c487e2626faf4de19fd3114a08ecf2
Cake <cake@cakey.moe>
2024-03-09 05:03:28 +0100

    Update a few portrait animations.

--- addons/dialogic/Modules/Character/DefaultAnimations/fade_out.gd
+++ addons/dialogic/Modules/Character/DefaultAnimations/fade_out.gd
@@ -3,14 +3,12 @@ extends DialogicAnimation
 func animate() -> void:
 	var tween := (node.create_tween() as Tween)
 
-	node.position.y = orig_pos.y + node.get_viewport().size.y/5
-	node.modulate.a = 0
+	node.modulate.a = 1.0
 	tween.set_ease(Tween.EASE_OUT)
 	tween.set_trans(Tween.TRANS_SINE)
 	tween.set_parallel()
 
-	tween.tween_property(node, 'position', orig_pos, time)
-	tween.tween_property(node, 'modulate:a', 1.0, time)
+	tween.tween_property(node, "modulate:a", 0.0, time)
 
 	await tween.finished
 	finished_once.emit()
