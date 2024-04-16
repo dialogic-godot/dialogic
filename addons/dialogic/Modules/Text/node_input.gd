9caa0cf1a1cf31dea7034d224ddfa989d6503c0e
Jowan-Spooner <raban-loeffler@posteo.de>
2024-04-17 00:20:09 +0200

    Attempt at improving glossary tag clicking & mouse input logic

--- addons/dialogic/Modules/Text/node_input.gd
+++ addons/dialogic/Modules/Text/node_input.gd
@@ -9,13 +9,13 @@ func _ready():
 	add_to_group('dialogic_input')
 	gui_input.connect(_on_gui_input)
 
+
 func _input(event: InputEvent) -> void:
 	if Input.is_action_pressed(ProjectSettings.get_setting('dialogic/text/input_action', 'dialogic_default_action')):
 		mouse_filter = Control.MOUSE_FILTER_STOP
 	else:
 		mouse_filter = Control.MOUSE_FILTER_IGNORE
 
+
 func _on_gui_input(event:InputEvent) -> void:
-	if Input.is_action_just_pressed(ProjectSettings.get_setting('dialogic/text/input_action', 'dialogic_default_action')):
-		if event is InputEventMouseButton and event.pressed:
-			DialogicUtil.autoload().Inputs.handle_input()
+	DialogicUtil.autoload().Inputs.handle_node_gui_input(event)
