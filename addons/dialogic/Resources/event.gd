6c0a71008cd3f4c6068e1df45a8629cdf010ce04
Jowan-Spooner <raban-loeffler@posteo.de>
2024-04-24 15:11:06 +0200

    Small property removal

--- addons/dialogic/Resources/event.gd
+++ addons/dialogic/Resources/event.gd
@@ -57,8 +57,6 @@ var event_color: Color = Color("FBB13C")
 var dialogic_color_name: String = ''
 ## To sort the buttons shown in the editor. Lower index is placed at the top of a category
 var event_sorting_index: int = 0
-## If false the name is not displayed on the event.
-var display_name: bool = true
 ## If true the event will not have a button in the visual editor sidebar
 var disable_editor_button: bool = false
 ## If false the event will hide it's body by default. Recommended for most events
