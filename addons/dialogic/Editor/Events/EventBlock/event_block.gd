f4aa44e67401ce038a3268f53d4ce898f006aacf
Jowan-Spooner <42868150+Jowan-Spooner@users.noreply.github.com>
2024-05-19 23:39:19 +0200

    Use contrast_color_1 for now (#2239)
    Make use of a color that exists in 4.2 and 4.3.

--- addons/dialogic/Editor/Events/EventBlock/event_block.gd
+++ addons/dialogic/Editor/Events/EventBlock/event_block.gd
@@ -64,9 +64,9 @@ func initialize_ui() -> void:
 
 	# Expand Button
 	%ToggleBodyVisibilityButton.icon = get_theme_icon("CodeFoldedRightArrow", "EditorIcons")
-	%ToggleBodyVisibilityButton.modulate = get_theme_color("icon_normal_color", "Editor")
-	%ToggleBodyVisibilityButton.set("theme_override_colors/icon_normal_color", get_theme_color("icon_normal_color", "Editor"))
-	%ToggleBodyVisibilityButton.set("theme_override_colors/icon_pressed_color", get_theme_color("icon_normal_color", "Editor"))
+	%ToggleBodyVisibilityButton.modulate = get_theme_color("contrast_color_1", "Editor")
+	%ToggleBodyVisibilityButton.set("theme_override_colors/icon_normal_color", get_theme_color("contrast_color_1", "Editor"))
+	%ToggleBodyVisibilityButton.set("theme_override_colors/icon_pressed_color", get_theme_color("contrast_color_1", "Editor"))
 
 	# Icon Panel
 	%IconPanel.tooltip_text = resource.event_name
