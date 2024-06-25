a6c0542c186176d1ebd26bf0ee514928036a3a2a
juliohq <JulioHenrique31501234@gmail.com>
2024-06-25 07:53:52 -0300

    Fix follow viewport export order

--- addons/dialogic/Modules/DefaultLayoutParts/Base_Default/default_layout_base.gd
+++ addons/dialogic/Modules/DefaultLayoutParts/Base_Default/default_layout_base.gd
@@ -4,13 +4,13 @@ extends DialogicLayoutBase
 ## The default layout base scene.
 
 @export var canvas_layer: int = 1
+@export var follow_viewport: bool = false
 
 @export_subgroup("Global")
 @export var global_bg_color: Color = Color(0, 0, 0, 0.9)
 @export var global_font_color: Color = Color("white")
 @export_file('*.ttf', '*.tres') var global_font: String = ""
 @export var global_font_size: int = 18
-@export var follow_viewport: bool = false
 
 
 func _apply_export_overrides() -> void:
