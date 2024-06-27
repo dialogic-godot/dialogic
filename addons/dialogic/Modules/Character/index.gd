4a0315cc21818e9edf883e1b08ab9643ef77da9f
Jowan-Spooner <raban-loeffler@posteo.de>
2024-06-27 16:01:30 +0200

    Add _get_portrait_scene_presets() to the indexer class
    This also indexes options for Default (empty string), Custom (custom path) and Simple Image portrait (the typical default portrait scene).

--- addons/dialogic/Modules/Character/index.gd
+++ addons/dialogic/Modules/Character/index.gd
@@ -18,3 +18,38 @@ func _get_text_effects() -> Array[Dictionary]:
 
 func _get_special_resources() -> Dictionary:
 	return {&'PortraitAnimation': list_animations("DefaultAnimations")}
+
+
+func _get_portrait_scene_presets() -> Array[Dictionary]:
+	return [
+		{
+			"path": "",
+			"name": "Default Scene",
+			"description": "The default scene defined in Settings>Portraits.",
+			"author":"Dialogic",
+			"type": "Default",
+			"icon":"",
+			"preview_image":[this_folder.path_join("default_portrait_thumbnail.png")],
+			"documentation":"",
+		},
+		{
+			"path": "CUSTOM",
+			"name": "Custom Scene",
+			"description": "A custom scene. Should extend DialogicPortrait and be in @tool mode.",
+			"author":"Dialogic",
+			"type": "Custom",
+			"icon":"",
+			"preview_image":[this_folder.path_join("custom_portrait_thumbnail.png")],
+			"documentation":"https://docs.dialogic.pro/custom-portraits.html",
+		},
+		{
+			"path": this_folder.path_join("default_portrait.tscn"),
+			"name": "Simple Image Portrait",
+			"description": "Can display images as portraits. Does nothing else.",
+			"author":"Dialogic",
+			"type": "General",
+			"icon":"",
+			"preview_image":[this_folder.path_join("simple_image_portrait_thumbnail.png")],
+			"documentation":"",
+		}
+	]
