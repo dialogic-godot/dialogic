3a85ac4078340a90e174afb47abdd708ff623643
Jowan-Spooner <raban-loeffler@posteo.de>
2024-07-04 19:15:11 +0200

    Fix and Improve edit fields of Choice Event

--- addons/dialogic/Modules/Choice/event_choice.gd
+++ addons/dialogic/Modules/Choice/event_choice.gd
@@ -27,7 +27,7 @@ var _has_condition := false
 
 #endregion
 
-var regex := RegEx.create_from_string(r'- (?<text>(?>\\\||(?(?=.*\|)[^|]|(?!\[if)[^|]))*)\|?\s*(\[if(?<condition>([^\]\[]|\[[^\]]*\])+)\])?\s*(?<shortcode>\[[^]]*\])?')
+var regex := RegEx.create_from_string(r'- (?<text>(?>\\\||(?(?=.*\|)[^|]|(?!\[if)[^|]))*)\|?\s*(\[if(?<condition>([^\]\[]|\[[^\]]*\])+)\])?\s*(\[(?<shortcode>[^]]*)\])?')
 
 #region EXECUTION
 ################################################################################
@@ -66,9 +66,9 @@ func to_text() -> String:
 
 	result_string = "- "+text.strip_edges()
 	var shortcode := store_to_shortcode_parameters()
-	if condition or shortcode:
+	if (condition and _has_condition) or shortcode:
 		result_string += " |"
-	if condition:
+	if condition and _has_condition:
 		result_string += " [if "+condition+"]"
 
 	if shortcode:
@@ -80,8 +80,8 @@ func from_text(string:String) -> void:
 	var result := regex.search(string.strip_edges())
 	if result == null:
 		return
-	text = result.get_string('text')
-	condition = result.get_string('condition')
+	text = result.get_string('text').strip_edges()
+	condition = result.get_string('condition').strip_edges()
 	_has_condition = not condition.is_empty()
 	if result.get_string('shortcode'):
 		load_from_shortcode_parameters(result.get_string("shortcode"))
@@ -129,8 +129,9 @@ func _get_property_original_translation(property:String) -> String:
 func build_event_editor() -> void:
 	add_header_edit("text", ValueType.SINGLELINE_TEXT, {'autofocus':true})
 	add_body_edit("", ValueType.LABEL, {"text":"Condition:"})
-	add_body_edit("_has_condition", ValueType.BOOL_BUTTON, {"editor_icon":["Add", "EditorIcons"]}, "not _has_condition")
-	add_body_edit("condition", ValueType.CONDITION, {}, "_has_condition")# {'left_text':'Condition:'})
+	add_body_edit("_has_condition", ValueType.BOOL_BUTTON, {"editor_icon":["Add", "EditorIcons"], "tooltip":"Add Condition"}, "not _has_condition")
+	add_body_edit("condition", ValueType.CONDITION, {}, "_has_condition")
+	add_body_edit("_has_condition", ValueType.BOOL_BUTTON, {"editor_icon":["Remove", "EditorIcons"], "tooltip":"Remove Condition"}, "_has_condition")
 	add_body_edit("else_action", ValueType.FIXED_OPTIONS, {'left_text':'Else:',
 		'options': [
 			{
@@ -145,7 +146,7 @@ func build_event_editor() -> void:
 				'label': 'Disable',
 				'value': ElseActions.DISABLE,
 			}
-		]}, '!condition.is_empty()')
+		]}, '_has_condition')
 	add_body_edit("disabled_text", ValueType.SINGLELINE_TEXT, {
 			'left_text':'Disabled text:',
 			'placeholder':'(Empty for same)'}, 'allow_alt_text()')
@@ -154,7 +155,7 @@ func build_event_editor() -> void:
 
 
 func allow_alt_text() -> bool:
-	return condition and (
+	return _has_condition and (
 		else_action == ElseActions.DISABLE or
 		(else_action == ElseActions.DEFAULT and
 		ProjectSettings.get_setting("dialogic/choices/def_false_behaviour", 0) == 1))
