185d745db2a7de152f47730b24ea361e22dde588
Jowan-Spooner <raban-loeffler@posteo.de>
2025-02-15 22:00:52 +0100

    Improve documentation on text subsystem

--- addons/dialogic/Modules/Text/event_text.gd
+++ addons/dialogic/Modules/Text/event_text.gd
@@ -127,9 +127,6 @@ func _execute() -> void:
 			_try_play_current_line_voice()
 			final_text = dialogic.Text.update_dialog_text(final_text, false, is_append)
 
-			# This is after the await for the textbox so it's after any text box 
-			# opening animations that may make animations reliant on knowing when
-			# the text starts 
 			dialogic.Text.text_started.emit({'text':final_text, 'character':character, 'portrait':portrait, 'append': is_append})
 
 			_mark_as_read(character_name_text, final_text)
