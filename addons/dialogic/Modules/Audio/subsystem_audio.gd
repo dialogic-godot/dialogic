92f4241be417e4ffbd850511a9be1b4530c120cd
Salia Nifo <salianifo@live.ca>
2025-06-08 14:04:44 -0400

    Multiple Bug Fixes (#2606)
    * Fix Play from Here
    
    * Fix portrait update not awaiting change_character_portrait
    
    * Fix audio loop default to true

--- addons/dialogic/Modules/Audio/subsystem_audio.gd
+++ addons/dialogic/Modules/Audio/subsystem_audio.gd
@@ -99,7 +99,7 @@ func update_audio(channel_name:= "", path := "", settings_overrides := {}) -> vo
 	## TODO use .merged after dropping 4.2 support
 	var audio_settings: Dictionary = DialogicUtil.get_audio_channel_defaults().get(channel_name, {})
 	audio_settings.merge(
-		{"volume":0, "audio_bus":"", "fade_length":0.0, "loop":false, "sync_channel":""}
+		{"volume":0, "audio_bus":"", "fade_length":0.0, "loop":true, "sync_channel":""}
 	)
 	audio_settings.merge(settings_overrides, true)
 
