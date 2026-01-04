671097d47dc34f3df4834b2477f20268bbe29f27
Jowan-Spooner <raban-loeffler@posteo.de>
2026-01-04 20:53:55 +0100

    Fix PlayTimeline tooltip

--- addons/dialogic/Editor/TimelineEditor/timeline_editor.gd
+++ addons/dialogic/Editor/TimelineEditor/timeline_editor.gd
@@ -34,7 +34,7 @@ func _register() -> void:
 	play_timeline_button = editors_manager.add_button(
 		get_theme_icon("PlayScene", "EditorIcons"),
 		"Play Timeline",
-		"Play the current timeline {0}".format("(Command+B)" if OS.get_name() == "macOS" else "(CTRL+F5)"),
+		"Play the current timeline {0}".format(["(Command+B)" if OS.get_name() == "macOS" else "(CTRL+F5)"]),
 		self,
 		editors_manager.ButtonPlacement.TOOLBAR_MAIN)
 	play_timeline_button.pressed.connect(play_timeline)
