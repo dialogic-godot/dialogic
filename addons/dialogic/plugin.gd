b923c3d3e1829af28a973ca88e6f48c5ed850185
zaknafean <zaknafean@gmail.com>
2026-01-12 14:56:13 -0500

    Striping PNGs from project exports
    Fix for #2658
    Thanks Nathan Hoad for a working example of using EditorExportPlugin and D for figuring out the filter
    
    Co-Authored-By: Nathan Hoad <78984+nathanhoad@users.noreply.github.com>

--- addons/dialogic/plugin.gd
+++ addons/dialogic/plugin.gd
@@ -11,7 +11,7 @@ const PLUGIN_INSPECTOR_PATH := "uid://bok1je25mskp7"
 ## References used by various other scripts to quickly reference these things
 var editor_view: Control  # the root of the dialogic editor
 var inspector_plugin: EditorInspectorPlugin = null
-
+var export_plugin: DialogicExportPlugin
 
 ## Initialization
 func _init() -> void:
@@ -37,6 +37,9 @@ func _enter_tree() -> void:
 	editor_view.plugin_reference = self
 	EditorInterface.get_editor_main_screen().add_child(editor_view)
 	_make_visible(false)
+	
+	export_plugin = DialogicExportPlugin.new()
+	add_export_plugin(export_plugin)
 
 	inspector_plugin = load(PLUGIN_INSPECTOR_PATH).new()
 	add_inspector_plugin(inspector_plugin)
