c21aadcf7ae86f5613cba9244be9886bd5f1fd83
zaknafean <zaknafean@gmail.com>
2026-01-15 16:24:29 -0500

    Background Layer options
    Fix for #2424
    Added adjustable settings to the background layer to override project settings for texture filtering and texture repeating.

--- addons/dialogic/Modules/DefaultLayoutParts/Layer_FullBackground/full_background_layer.gd
+++ addons/dialogic/Modules/DefaultLayoutParts/Layer_FullBackground/full_background_layer.gd
@@ -1,2 +1,31 @@
 @tool
 extends DialogicLayoutLayer
+
+# Awkward way of doing this, but we need a 'none' default to preserve compatiblity
+# These enums are just copies of Viewport.DefaultCanvasItemTextureFilter
+enum DefaultCanvasItemTextureFilter {
+	USE_PROJECT_SETTINGS,
+	NEAREST,
+	LINEAR,
+	LINEAR_WITH_MIPMAPS,
+	NEAREST_WITH_MIPMAPS
+}
+
+# These enums are just copies of Viewport.DefaultCanvasItemTextureRepeat
+enum DefaultCanvasItemTextureRepeat {
+	USE_PROJECT_SETTINGS,
+	DISABLED,
+	ENABLED,
+	REPEAT_MIRROR
+}
+
+
+@export var bg_texture_filter : DefaultCanvasItemTextureFilter = DefaultCanvasItemTextureFilter.USE_PROJECT_SETTINGS
+@export var bg_texture_repeat : DefaultCanvasItemTextureRepeat = DefaultCanvasItemTextureRepeat.USE_PROJECT_SETTINGS
+
+
+func _ready() -> void:
+	# This is a work around to the style editor not accepting negative numbered enums
+	# This allows the enum values to properly match the Viewport defaults
+	bg_texture_filter = bg_texture_filter -1
+	bg_texture_repeat = bg_texture_repeat -1
