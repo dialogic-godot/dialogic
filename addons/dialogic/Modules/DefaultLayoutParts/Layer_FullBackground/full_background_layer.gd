a382d28f9902d7100347a97981d6b4dcddab3d00
Jowan-Spooner <raban-loeffler@posteo.de>
2026-01-18 00:39:46 +0100

    Allow for enums with any sort of indexing in style editor

--- addons/dialogic/Modules/DefaultLayoutParts/Layer_FullBackground/full_background_layer.gd
+++ addons/dialogic/Modules/DefaultLayoutParts/Layer_FullBackground/full_background_layer.gd
@@ -4,28 +4,21 @@ extends DialogicLayoutLayer
 # Awkward way of doing this, but we need a 'none' default to preserve compatiblity
 # These enums are just copies of Viewport.DefaultCanvasItemTextureFilter
 enum DefaultCanvasItemTextureFilter {
-	USE_PROJECT_SETTINGS,
-	NEAREST,
-	LINEAR,
-	LINEAR_WITH_MIPMAPS,
-	NEAREST_WITH_MIPMAPS
+	USE_PROJECT_SETTINGS=-1,
+	NEAREST=0,
+	LINEAR=1,
+	LINEAR_WITH_MIPMAPS=2,
+	NEAREST_WITH_MIPMAPS=3
 }
 
 # These enums are just copies of Viewport.DefaultCanvasItemTextureRepeat
 enum DefaultCanvasItemTextureRepeat {
-	USE_PROJECT_SETTINGS,
-	DISABLED,
-	ENABLED,
-	REPEAT_MIRROR
+	USE_PROJECT_SETTINGS=-1,
+	DISABLED=0,
+	ENABLED=1,
+	REPEAT_MIRROR=2
 }
 
 
 @export var bg_texture_filter : DefaultCanvasItemTextureFilter = DefaultCanvasItemTextureFilter.USE_PROJECT_SETTINGS
 @export var bg_texture_repeat : DefaultCanvasItemTextureRepeat = DefaultCanvasItemTextureRepeat.USE_PROJECT_SETTINGS
-
-
-func _ready() -> void:
-	# This is a work around to the style editor not accepting negative numbered enums
-	# This allows the enum values to properly match the Viewport defaults
-	bg_texture_filter = bg_texture_filter -1
-	bg_texture_repeat = bg_texture_repeat -1
